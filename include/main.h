a842531774ff1039e044dd05bb089272c1397405
baylessj <baylessj@purdue.edu>
2018-09-16 17:00:10 -0400

    🚸 Add public declarations for comp control tasks
    Summary: It's much easier to test `autonomous()` when it can be called from `opcontrol()`, and this makes that possible. Matches PROS 2 functionality.
    
    Test Plan:
    [x] Test call to autonomous in C
    ```
    void autonomous() {
    	lcd_set_text(2, "Hello from Auton!");
    }
    ```
    ```
    void opcontrol() {
      delay(2000);
      autonomous();
    }
    ```
    
    [x] Test call to autonomous in C++
    ```
    void autonomous() {
    	pros::lcd::set_text(2, "Hello from Auton!");
    }
    ```
    ```
    void opcontrol() {
      pros::delay(2000);
      autonomous();
    }
    ```
    
    Reviewers: O1 The Ori, berman5
    
    Reviewed By: O1 The Ori, berman5
    
    Subscribers: berman5
    
    Tags: #zorp
    
    Differential Revision: https://phabricator.purduesigbots.com/D188

--- include/main.h
+++ include/main.h
@@ -54,6 +54,23 @@
 // using namespace pros::literals;
 // using namespace okapi;
 
+/**
+ * Prototypes for the competition control tasks are redefined here to ensure
+ * that they can be called from user code (i.e. calling autonomous from a
+ * button press in opcontrol() for testing purposes).
+ */
+#ifdef __cplusplus
+extern "C" {
+#endif
+void autonomous(void);
+void initialize(void);
+void disabled(void);
+void competition_initialize(void);
+void opcontrol(void);
+#ifdef __cplusplus
+}
+#endif
+
 #ifdef __cplusplus
 /**
  * You can add C++-only headers here
