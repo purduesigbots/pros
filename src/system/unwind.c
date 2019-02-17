f1e47cd35c40e5f55aa96a2cba87c25eff32d0ff
Elliot Berman <berman5@purdue.edu>
2019-02-16 20:23:49 -0500

    Cleanup

--- src/system/unwind.c
+++ src/system/unwind.c
@@ -14,7 +14,7 @@
  */
 
 #include <unwind.h>
-#include <setjmp.h>
+#include <malloc.h>
 
 #include "rtos/task.h"
 #include "rtos/tcb.h"
@@ -37,17 +37,9 @@ extern __EIT_entry __exidx_end;
 // 	return &__exidx_start;
 // }
 
-#define REGISTER_BASE		67
-//
 extern void task_clean_up();
 
 _Unwind_Reason_Code trace_fn(_Unwind_Context* unwind_ctx, void* d) {
-	// printf("trace_fn: %x\n", d);
-	// int* depth = (int*)d;
-	// phase2_vrs* vrs = (phase2_vrs*)unwind_ctx;
-	// for(uint8_t i = 0; i < 16; i++) {
-	// 	printf("r%d: %x\n", i, vrs->core.r[i]);
-	// }
 	uint32_t pc = _Unwind_GetIP(unwind_ctx);
 	printf("\t0x%08lx\n", pc);
 	if (pc == (uint32_t)task_clean_up) {
@@ -64,60 +56,6 @@ struct core_regs
 {
   _uw r[16];
 };
-
-/* We use normal integer types here to avoid the compiler generating
-   coprocessor instructions.  */
-struct vfp_regs
-{
-  _uw64 d[16];
-  _uw pad;
-};
-
-struct vfpv3_regs
-{
-  /* Always populated via VSTM, so no need for the "pad" field from
-     vfp_regs (which is used to store the format word for FSTMX).  */
-  _uw64 d[16];
-};
-
-struct wmmxd_regs
-{
-  _uw64 wd[16];
-};
-
-struct wmmxc_regs
-{
-  _uw wc[4];
-};
-
-/* The ABI specifies that the unwind routines may only use core registers,
-   except when actually manipulating coprocessor state.  This allows
-   us to write one implementation that works on all platforms by
-   demand-saving coprocessor registers.
-   During unwinding we hold the coprocessor state in the actual hardware
-   registers and allocate demand-save areas for use during phase1
-   unwinding.  */
-
-typedef struct
-{
-  /* The first fields must be the same as a phase2_vrs.  */
-  _uw demand_save_flags;
-  struct core_regs core;
-  _uw prev_sp; /* Only valid during forced unwinding.  */
-  struct vfp_regs vfp;
-  struct vfpv3_regs vfp_regs_16_to_31;
-  struct wmmxd_regs wmmxd;
-  struct wmmxc_regs wmmxc;
-} phase1_vrs;
-
-#define DEMAND_SAVE_VFP 1	/* VFP state has been saved if not set */
-#define DEMAND_SAVE_VFP_D 2	/* VFP state is for FLDMD/FSTMD if set */
-#define DEMAND_SAVE_VFP_V3 4    /* VFPv3 state for regs 16 .. 31 has
-                                   been saved if not set */
-#define DEMAND_SAVE_WMMXD 8	/* iWMMXt data registers have been
-				   saved if not set.  */
-#define DEMAND_SAVE_WMMXC 16	/* iWMMXt control registers have been
-				   saved if not set.  */
 
 /* This must match the structure created by the assembly wrappers.  */
 typedef struct
@@ -140,7 +78,7 @@ static inline void print_phase2_vrs(phase2_vrs* vrs) {
 
 // recover registers from a data abort. Specific knowledge about callee stack
 // used from FreeRTOS_DataAbortHandler and it calling DataAbortInterrupt
-void recover_vrs_from_data_abort(_uw* sp, phase2_vrs* vrs) {
+void p2vrs_from_data_abort(_uw* sp, phase2_vrs* vrs) {
   // sp is stack pointer when FreeRTOS_DataAbortHandler invokes DataAbortInterrupt
 	vrs->demand_save_flags = 0;
   // start pulling these registers off the stack
@@ -159,21 +97,28 @@ void recover_vrs_from_data_abort(_uw* sp, phase2_vrs* vrs) {
 
 void backtrace_from_data_abort(void* _sp) {
   phase2_vrs vrs;
-  recover_vrs_from_data_abort((_uw*)_sp, &vrs);
+  p2vrs_from_data_abort((_uw*)_sp, &vrs);
   puts("DATA ABORT EXCEPTION\n");
   puts("REGISTERS AT ABORT");
 	print_phase2_vrs(&vrs);
-  _Unwind_Control_Block ucbp;
-  ucbp.unwinder_cache.reserved1 = 0;
 
   puts("BEGIN STACK TRACE");
   __gnu_Unwind_Backtrace(trace_fn, NULL, &vrs);
 	puts("END OF TRACE");
-  // extern task_t competition_task;
-  // backtrace_task(competition_task);
+
+  puts("HEAP USAGE");
+	struct mallinfo info = mallinfo();
+  printf("Used bytes: %d\tFree bytes: %d\n", info.uordblks, info.fordblks);
 }
 
-static inline phase2_vrs create_phase2_vrs(task_t task) {
+// /******************************************************************************/
+// /**               Modified RTOS-Task Target Unwinder Functions               **/
+// /**                                                                          **/
+// /** These functions use the __gnu_Unwind_* functions providing our helper    **/
+// /** functions and phase2_vrs structure based on a target task                **/
+// /******************************************************************************/
+#define REGISTER_BASE		67
+static inline phase2_vrs p2vrs_from_task(task_t task) {
 	// should be called with the task scheduler suspended
 	taskENTER_CRITICAL();
 
@@ -215,143 +160,10 @@ static inline phase2_vrs create_phase2_vrs(task_t task) {
 	return vrs;
 }
 
-// struct unwind_cleanup {
-// 	jmp_buf env;
-// 	int depth;
-// 	TCB_t* tcb;
-// };
-//
-// extern void unwind_setjmp(struct unwind_cleanup* ptr, void* ret);
-//
-// extern void* __cxa_allocate_exception(size_t size);
-// extern _Unwind_Reason_Code __gnu_Unwind_Backtrace(_Unwind_Trace_Fn trace, void* trace_argument, phase2_vrs* entry_vrs);
-// extern _Unwind_Reason_Code __gnu_Unwind_ForcedUnwind(_Unwind_Control_Block* ucbp, _Unwind_Stop_Fn stop_fn, void* arg, phase2_vrs* ptr);
-//
-// /******************************************************************************/
-// /**                         Unwinder helper functions                        **/
-// /******************************************************************************/
-//
-// _Unwind_Reason_Code
-// test_forced_unwind (_Unwind_Trace_Fn trace, void * trace_argument,
-// 		       phase2_vrs * entry_vrs)
-// {
-// 	printf("arm's vrs:\n");
-// 	for(uint8_t i = 0; i < 16; i++) {
-// 		printf("r%d: %x\n", i, entry_vrs->core.r[i]);
-// 	}
-// 	return 0;
-// 	// return __gnu_Unwind_ForcedUnwind(ucbp, stop_fn, stop_arg, entry_vrs);
-// }
-//
-// _Unwind_Reason_Code unwind_stop_fn(int i, _Unwind_Action act, _Unwind_Exception_Class cls,
-// 	_Unwind_Control_Block * ucbp, struct _Unwind_Context * unwind_ctx, void * arg) {
-// 	uint32_t pc = _Unwind_GetIP(unwind_ctx);
-// 	fprintf(stdout, "\t0x%08lx\n", pc);
-// 	if (pc == (uint32_t)task_clean_up) {
-// 		printf("done %d\n", __LINE__);
-// 		longjmp(((struct unwind_cleanup*)arg)->env, 1);
-// 		return _URC_FAILURE;
-// 	}
-// 	fprintf(stdout, "continuing to unwind\n");
-// 	task_delay(2);
-// 	return _URC_NO_REASON;
-// }
-//
-// void exception_cleanup(_Unwind_Reason_Code code, _Unwind_Control_Block* ucb) {
-// 	printf("Cleanup this damn exception %d\n", code);
-// 	printf("%x\n", ucb->pr_cache.fnstart);
-// 	printf("%x %x %x %x %x %x %x %x \n", ucb->exception_class[0],
-// 	ucb->exception_class[1],
-// 	ucb->exception_class[2],
-// 	ucb->exception_class[3],
-// 	ucb->exception_class[4],
-// 	ucb->exception_class[5],
-// 	ucb->exception_class[6],
-// 	ucb->exception_class[7]);
-// }
-
-//
-//
-// // static inline void print_unwind_cleanup(struct unwind_cleanup* cleanup) {
-// // 	static const char registers[16][4] = {
-// // 		"r0", "r1", "r2", "r3", "r4", "r5", "r6", "r7", "r8", "r9", "r10", "r11", "r12", "sp", "lr", "pc"
-// // 	};
-// // 	for(size_t i = 0; i < 15; i++) {
-// // 		printf("%3s: %#x\n", registers[i], cleanup->r[i]);
-// // 	}
-// // 	printf("cpsr: %#x\n depth: %d\n", cleanup->cpsr, cleanup->depth);
-// // }
-//
-// /******************************************************************************/
-// /**                        Unwinder "Native" Functions                       **/
-// /**                                                                          **/
-// /** These functions use the _Unwind_* functions directly providing our       **/
-// /** helper functions                                                         **/
-// /******************************************************************************/
-// void backtrace_self() {
-// 	int depth = 0;
-// 	_Unwind_Backtrace(trace_fn, &depth);
-// }
-//
-// void unwind_self() {
-// 	int depth = 0;
-// 	_Unwind_Control_Block* ucbp = __cxa_allocate_exception(sizeof *ucbp);
-// 	memcpy(ucbp->exception_class, "ARM\0PROS", 8);
-// 	ucbp->exception_cleanup = exception_cleanup;
-// 	ucbp->unwinder_cache.reserved1 = 0;
-// 	_Unwind_ForcedUnwind(ucbp, unwind_stop_fn, &depth);
-// }
-//
-// /******************************************************************************/
-// /**               Modified RTOS-Task Target Unwinder Functions               **/
-// /**                                                                          **/
-// /** These functions use the __gnu_Unwind_* functions providing our helper    **/
-// /** functions and phase2_vrs structure based on a target task                **/
-// /******************************************************************************/
 void backtrace_task(task_t task) {
-	phase2_vrs vrs = create_phase2_vrs(task);
+	phase2_vrs vrs = p2vrs_from_task(task);
   printf("Trace:\n");
   _Unwind_Control_Block ucbp;
 	__gnu_Unwind_Backtrace(trace_fn, NULL, &vrs);
 	printf("finished trace\n");
 }
-//
-// void unwind_task(task_t task) {
-// 	// performs a half-hearted context switch that sets up the link register and stack pointer
-// 	TCB_t* tcb = (TCB_t*)task;
-// 	if(tcb == NULL) {
-// 		tcb = pxCurrentTCB;
-// 	}
-//
-// 	_Unwind_Control_Block* ucbp = __cxa_allocate_exception(sizeof *ucbp);
-// 	memcpy(ucbp->exception_class, "ARM\0PROS", 8);
-// 	ucbp->exception_cleanup = exception_cleanup;
-// 	ucbp->unwinder_cache.reserved1 = 0;
-//
-// 	phase2_vrs vrs = create_phase2_vrs(task);
-//
-// 	struct unwind_cleanup cleanup;
-// 	cleanup.depth = 0;
-// 	int result = setjmp(cleanup.env);
-// 	printf("Howdy\n");
-// 	if(result == 0) {
-// 		printf("unwinding!\n");
-// 		__gnu_Unwind_ForcedUnwind(ucbp, unwind_stop_fn, &cleanup, &vrs);
-// 	}
-//
-// 	// __gnu_Unwind_ForcedUnwind(ucbp, unwind_stop_fn, &depth, &vrs);
-// 	printf("finished unwinding\n");
-// }
-//
-// // debugging function to print out important information from a task's stack
-// // for figuring out its current state
-// void print_stack(task_t task) {
-// 	TCB_t* tcb = (TCB_t*)task;
-// 	if(tcb == NULL) {
-// 		tcb = pxCurrentTCB;
-// 	}
-//
-// 	for(int i = -10 ; i < 50; i++) {
-// 		printf("67 + %d/%#lx: %#lx\n", i, (unsigned long)(tcb->pxTopOfStack + 67 + i), tcb->pxTopOfStack[67 + i]);
-// 	}
-// }
