a5f40c0313b8dc4a1c4ff08653b13a16200d4472
Alex Brooke <akb.sbc@gmail.com>
2019-09-02 20:46:22 -0400

    add documentation comment for callback function

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,11 @@
 #include "main.h"
 
+/**
+ * A callback function for LLEMU's center button.
+ *
+ * When this callback is fired, it will toggle line 2 of the LCD text between
+ * "I was pressed!" and nothing.
+ */
 void on_center_button() {
 	static bool pressed = false;
 	pressed = !pressed;
