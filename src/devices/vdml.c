be07299fb77cf534eb546d41c291daaabb421ff0
Jerry lum <serverofjerry@gmail.com>
2019-09-13 04:05:51 +0800

    🐛 Return proper error code for VDML functions that return floating point numbers (#160)
    * :bug: Fix problem of return PROS_ERR_F error code
    
    * :bug: Improve condition statement and comment VDML function
    
    
    🤫 forgot to change
    
    * :bug: Undo claim_port_try code to avoid warning
    
    
    
    
    :bug: Undo claim_port_try code to avoid warning

--- src/devices/vdml.c
+++ src/devices/vdml.c
@@ -32,14 +32,14 @@ extern void port_mutex_init();
 int32_t claim_port_try(uint8_t port, v5_device_e_t type) {
 	if (!VALIDATE_PORT_NO(port)) {
 		errno = ENXIO;
-		return PROS_ERR;
+		return 0;
 	}
 	if (registry_validate_binding(port, type) != 0) {
-		return PROS_ERR;
+		return 0;
 	}
 	if (!port_mutex_take(port)) {
 		errno = EACCES;
-		return PROS_ERR;
+		return 0;
 	}
 	return 1;
 }
