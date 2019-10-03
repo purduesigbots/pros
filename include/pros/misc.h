59bde8f34506677ebd16b4935380925017a9d2e2
Andrew Strauss <astrauss11@gmail.com>
2019-10-02 22:01:03 -0400

    Change controller_clear

--- include/pros/misc.h
+++ include/pros/misc.h
@@ -337,8 +337,7 @@ int32_t controller_clear_line(controller_id_e_t id, uint8_t line);
  * Clears all of the lines on the controller screen.
  *
  * \note Controller text setting is currently in beta, so continuous, fast
- * updates will not work well. To be able to clear all 3 lines, the function
- * blocks for a total of 110ms.
+ * updates will not work well.
  *
  * This function uses the following values of errno when an error state is
  * reached:
--- src/devices/controller.c
+++ src/devices/controller.c
@@ -289,14 +289,7 @@ int32_t controller_clear_line(controller_id_e_t id, uint8_t line) {
 }
 
 int32_t controller_clear(controller_id_e_t id) {
-	for (int i = 0; i < 3; i++) {
-		int32_t rtn = controller_clear_line(id, i);
-		if (rtn == PROS_ERR)
-			return PROS_ERR;
-		if (i != 2)
-			delay(55);
-	}
-	return 1;
+	return controller_print(id, 0, 0, "");
 }
 
 int32_t controller_rumble(controller_id_e_t id, const char* rumble_pattern) {
