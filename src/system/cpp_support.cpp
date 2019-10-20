3c52273abcdc9e28a487c9b41c83d0a63ae29685
Theo Lemay <theol0403@gmail.com>
2019-10-20 11:38:51 -0700

    🚸 Clean up screen printing

--- src/system/cpp_support.cpp
+++ src/system/cpp_support.cpp
@@ -27,17 +27,17 @@ extern "C" void task_fn_wrapper(task_fn_t fn, void* args) {
 #ifdef __cpp_exceptions
 	} catch (const std::runtime_error& re) {
 		fprintf(stderr, "Runtime error: %s", re.what(), "\n");
-		vexDisplayString(5, "an runtime error occured:");
+		vexDisplayString(5, "An runtime error occured:");
 		vexDisplayString(6, "%s", re.what());
-		vexDisplayString(7, "open terminal to see full error message");
+		vexDisplayString(7, "Note: open terminal for error message");
 	} catch (const std::exception& ex) {
 		fprintf(stderr, "Exception occurred: %s", ex.what(), "\n");
-		vexDisplayString(5, "an exception occured:");
+		vexDisplayString(5, "An exception occured:");
 		vexDisplayString(6, "%s", ex.what());
-		vexDisplayString(7, "open terminal to see full error message");
+		vexDisplayString(7, "Note: open terminal to see full error message");
 	} catch (...) {
-		fprintf(stderr, "Unknown error occurred. Possible memory corruption");
-		vexDisplayString(5, "an unknown error occurred");
+		fprintf(stderr, "Unknown error occurred. Possible memory corruption\n");
+		vexDisplayString(5, "An unknown error occurred");
 	}
 #endif
 }
