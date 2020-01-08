ae9ecf4d61fb760a9ab2bae05022edd1a4cbc359
nathan-moore <nathanmoore424@gmail.com>
2020-01-08 16:15:05 -0500

    📝 Update copyright (#193)

--- include/pros/api_legacy.h
+++ include/pros/api_legacy.h
@@ -10,7 +10,7 @@
  * This file should not be modified by users, since it gets replaced whenever
  * a kernel upgrade occurs.
  *
- * Copyright (c) 2017-2019, Purdue University ACM SIGBots.
+ * Copyright (c) 2017-2020, Purdue University ACM SIGBots.
  * All rights reserved.
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
