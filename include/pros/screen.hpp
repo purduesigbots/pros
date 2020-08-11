8159359c328f91bc6d5a7e6f490742f73e4d5412
WillXuCodes <xu1321@purdue.edu>
2020-08-11 00:13:11 -0400

    Forgot to add the actual files in the old commit.

---
+++ include/pros/screen.hpp
@@ -0,0 +1,262 @@
+/*
+ * \file screen.hpp
+ *
+ * \brief Brain screen display and touch functions.
+ *
+ * Contains user calls to the v5 screen for touching and displaying graphics.
+ *
+ * \copyright (c) 2017, Purdue University ACM SIGBots.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef _PROS_SCREEN_HPP_
+#define _PROS_SCREEN_HPP_
+
+#include "pros/screen.h"
+
+namespace pros {
+class screen {
+    public:
+    /**
+     * \brief Set the pen color for subsequent graphics operations
+     *
+     * \param[in] color	The pen color to set (it is recommended to use values
+     * 		 from the enum defined in colors.h)
+     */
+    void set_pen(uint32_t color);
+
+    /**
+     * \brief Set the eraser color for clearing and the current background.
+     *
+     * \param[in] color	The eraser color to set (it is recommended to use values
+     * 					from the enum defined in colors.h)
+     */
+    void set_eraser(uint32_t color);
+
+    /**
+     * \brief Reset the display to the default black screen
+     */
+    void clear_screen(void);
+
+    /**
+     * \brief Scroll lines on the display upwards.
+     *
+     * \param[in] start_line	The line from which scrolling will start
+     * \param[in] lines			The number of lines to scroll up
+     */
+    void scroll(int16_t start_line, int16_t lines);
+
+    /**
+     * \brief Scroll lines within a region on the display
+     *
+     * This function behaves in the same way as `screen_scroll`, except that you
+     * specify a rectangular region within which to scroll lines instead of a start
+     * line.
+     *
+     * \param[in] x0, y0	The (x,y) coordinates of the first corner of the
+     * 						rectangular region
+     * \param[in] x1, y1	The (x,y) coordinates of the second corner of the
+     * 						rectangular region
+     * \param[in] lines 	The number of lines to scroll upwards
+     */
+    void scroll_area(int16_t x0, int16_t y0, int16_t x1, int16_t y1, int16_t lines);
+
+    /**
+     * \brief Copy a screen region (designated by a rectangle) from an off-screen buffer 
+     * to the screen
+     *
+     * \param[in] x0, y0 	The (x,y) coordinates of the first corner of the
+     * 						rectangular region of the screen
+     * \param[in] x1, y1	The (x,y) coordinates of the second corner of the
+     * 						rectangular region of the screen
+     * \param[in] buf		Off-screen buffer containing screen data
+     * \param[in] stride	Off-screen buffer width in pixels, such that image size
+     * 						is stride-padding
+     */
+    void copy_area(int16_t x0, int16_t y0, int16_t x1, int16_t y1, uint32_t* buf, int32_t stride);
+
+    /**
+     * \brief Draw a single pixel on the screen using the current pen color
+     *
+     * \param[in] x, y 	The (x,y) coordinates of the pixel
+     */
+    void draw_pixel(int16_t x, int16_t y);
+
+    /**
+     * \brief Clear a pixel from the screen (Sets the location)
+     *
+     * \param[in] x, y 	The (x,y) coordinates of the cleared
+     */
+    void clear_pixel(int16_t x, int16_t y);
+
+    /**
+     * \brief Draw a line on the screen using the current pen color
+     *
+     * \param[in] x0, y0	The (x, y) coordinates of the first point of the line
+     * \param[in] x1, y1 	The (x, y) coordinates of the second point of the line
+     */
+    void draw_line(int16_t x0, int16_t y0, int16_t x1, int16_t y1);
+
+    /**
+     * \brief Draw a line on the screen using the current background color
+     *
+     * \param[in] x0, y0	The (x, y) coordinates of the first point of the line
+     * \param[in] x1, y1 	The (x, y) coordinates of the second point of the line
+     */
+    void clear_line(int16_t x0, int16_t y0, int16_t x1, int16_t y1);
+
+    /**
+     * \brief Draw a rectangle on the screen using the current pen color
+     *
+     * \param[in] x0, y0 	The (x,y) coordinates of the first point of the rectangle
+     * \param[in] x1, y1 	The (x,y) coordinates of the second point of the rectangle
+     */
+    void draw_rect(int16_t x0, int16_t y0, int16_t x1, int16_t y1);
+
+    /**
+     * \brief Draw a rectangle on the screen using the current eraser color
+     *
+     * \param[in] x0, y0 	The (x,y) coordinates of the first point of the rectangle
+     * \param[in] x1, y1 	The (x,y) coordinates of the second point of the rectangle
+     */
+    void clear_rect(int16_t x0, int16_t y0, int16_t x1, int16_t y1);
+
+    /**
+     * \brief Fill a rectangular region of the screen using the current pen
+     * 		  color
+     *
+     * \param[in] x0, y0 	The (x,y) coordinates of the first point of the rectangle
+     * \param[in] x1, y1 	The (x,y) coordinates of the second point of the rectangle
+     */
+    void fill_rect(int16_t x0, int16_t y0, int16_t x1, int16_t y1);
+
+    /**
+     * \brief Draw a circle on the screen using the current pen color
+     *
+     * \param[in] x, y 	The (x,y) coordinates of the center of the circle
+     * \param[in] r 	The radius of the circle
+     */
+    void draw_circle(int16_t x, int16_t y, int16_t r);
+
+    /**
+     * \brief Draw a circle on the screen using the current eraser color
+     *
+     * \param[in] x, y 	The (x,y) coordinates of the center of the circle
+     * \param[in] r 	The radius of the circle
+     */
+    void clear_circle(int16_t x, int16_t y, int16_t r);
+
+    /**
+     * \brief Fill a circular region of the screen using the current pen
+     * 		  color
+     *
+     * \param[in] x, y 	The (x,y) coordinates of the center of the circle
+     * \param[in] r 	The radius of the circle
+     */
+    void fill_circle(int16_t x, int16_t y, int16_t r);
+
+    /******************************************************************************/
+    /**                       Screen Text Display Functions                      **/
+    /**                                                                          **/
+    /**     These functions allow programmers to display text on the v5 screen   **/
+    /******************************************************************************/
+
+    /**
+     * \brief Print a string to the screen on the specified line
+     *
+ * \param[in] txt_fmt Text format enum that determines if the text is small, large, normal, and/or centered.
+     * \param[in] line The line number on which to print
+     * \param[in] text The text to display
+     */
+    void print(text_format txt_fmt, const int16_t line, const char* text);
+
+    /**
+     * \brief Print a string to the screen at the specified coordinates
+     *
+     * \param[in] txt_fmt Text format enum that determines if the text is small, large, normal, and/or centered.
+     * \param[in] x, y The (x,y) coordinates of the top left corner of the string
+     * \param[in] text The text to display
+     */
+    void print_at(text_format txt_fmt, int16_t x, int16_t y, const char* text);
+
+    /**
+     * \brief Print a formatted string to the screen on the specified line
+     * 
+     * \param[in] txt_fmt Text format enum that determines if the text is small, large, normal, and/or centered.
+     * \param[in] line The line number on which to print
+     * \param[in] fmt  Format string
+     * \param[in] ...  Optional list of arguments for the format string
+     */
+    void print_formatted(text_format txt_fmt, const int16_t line, const char* fmt, ...);
+
+    /**
+     * \brief Print a formatted string to the screen at the specified point
+     *
+     * \param[in] txt_fmt Text format enum that determines if the text is small, large, normal, and/or centered.
+     * \param[in] x, y The (x,y) coordinates of the top left corner of the string
+     * \param[in] fmt  Format string
+     * \param[in] ...  Optional list of arguments for the format string
+     */
+    void print_formatted_at(text_format txt_fmt, int16_t x, int16_t y, const char* fmt, ...);
+
+    /**
+     * \brief Print a formatted string to the screen on the specified line
+     *
+     * Same as `display_printf` except that this uses a `va_list` instead of the
+     * ellipsis operator so this can be used by other functions.
+     *
+     * \param[in] txt_fmt Text format enum that determines if the text is small, large, normal, and/or centered.
+     * \param[in] line The line number on which to print
+     * \param[in] fmt  Format string
+     * \param[in] args List of arguments for the format string
+     */
+    void vprint_formatted(text_format txt_fmt, const int16_t line, const char* fmt, va_list args);
+
+    /**
+     * \brief Print a formatted string to the screen at the specified coordinates
+     *
+     * Same as `display_printf_at` except that this uses a `va_list` instead of the
+     * ellipsis operator so this can be used by other functions.
+     * 
+     * \param[in] txt_fmt Text format enum that determines if the text is small, large, normal, and/or centered.
+     * \param[in] x, y The (x,y) coordinates of the top left corner of the string
+     * \param[in] fmt  Format string
+     * \param[in] args List of arguments for the format string
+     */
+    void vprint_formatted_at(text_format txt_fmt, int16_t x, int16_t y, const char* fmt, va_list args);
+
+/******************************************************************************/
+/**                         Screen Touch Functions                           **/
+/**                                                                          **/
+/**               These functions allow programmers to access                **/
+/**                    information about screen touches                      **/
+/******************************************************************************/
+
+    /**
+     * \brief Gets the x coordinate of the last touch.
+     * 
+     * \return The x coordinate of the last touch.
+     */
+    int16_t last_x(void);
+
+    /**
+     * \brief Gets the y coordinate of the last touch.
+     * 
+     * \return The y coordinate of the last touch.
+     */
+    int16_t last_y(void);
+
+    /**
+     * \brief Gets the touch status of the last touch of the screen.
+     * 
+     * \return The touch_event_e_t enum specifier that indicates the last touch status of the screen (E_TOUCH_EVENT_RELEASE, E_TOUCH_EVENT_PRESS, or E_TOUCH_EVENT_PRESS_AND_HOLD). 
+     */
+    touch_event_e_t last_touch_status(void);
+
+}; //class screen
+} //namespace pros
+
+#endif //header guard
---
+++ src/display/screen.c
@@ -0,0 +1,285 @@
+/**
+ * \file screen.c
+ *
+ * \brief Brain screen display and touch functions.
+ *
+ * Contains user calls to the v5 screen for touching and displaying graphics.
+ *
+ * \copyright (c) 2017, Purdue University ACM SIGBots.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include "pros/screen.h"
+#include "graphics.c"
+/******************************************************************************/
+/**                  Screen Graphical Display Functions                      **/
+/**                                                                          **/
+/**   These functions allow programmers to display shapes on the v5 screen   **/
+/******************************************************************************/
+
+void screen_set_pen(uint32_t color){
+    display_set_color_fg(color);
+}
+
+void screen_set_eraser(uint32_t color){
+    display_set_color_bg(color);
+}
+
+void screen_clear(void){
+    display_erase();
+}
+
+void screen_scroll(int16_t start_line, int16_t lines){
+    display_scroll(start_line,lines);
+}
+
+void screen_scroll_area(int16_t x0, int16_t y0, int16_t x1, int16_t y1, int16_t lines){
+    display_scroll_rect(x0,y0,x1,y1,lines);
+}
+
+void screen_copy_area(int16_t x0, int16_t y0, int16_t x1, int16_t y1, uint32_t* buf, int32_t stride){
+    display_copy_rect(x0, y0, x1, y1, buf, stride);
+}
+
+void screen_draw_pixel(int16_t x, int16_t y){
+    display_set_pixel_fg(x,y);
+}
+
+void screen_clear_pixel(int16_t x, int16_t y){
+    display_set_pixel_bg(x,y);
+}
+
+void screen_draw_line(int16_t x0, int16_t y0, int16_t x1, int16_t y1){
+    display_draw_line( x0, y0, x1, y1);
+}
+
+void screen_clear_line(int16_t x0, int16_t y0, int16_t x1, int16_t y1){
+    display_clear_line( x0, y0, x1, y1);
+}
+
+void screen_draw_rect(int16_t x0, int16_t y0, int16_t x1, int16_t y1){
+    display_draw_rect( x0, y0, x1, y1);
+}
+
+void screen_clear_rect(int16_t x0, int16_t y0, int16_t x1, int16_t y1){
+    display_clear_rect(x0, y0, x1, y1);
+}
+
+void screen_fill_rect(int16_t x0, int16_t y0, int16_t x1, int16_t y1){
+    display_fill_rect(x0, y0, x1, y1);
+}
+
+void screen_draw_circle(int16_t x, int16_t y, int16_t radius){
+    display_draw_circle(x,y,radius);
+}
+
+void screen_clear_circle(int16_t x, int16_t y, int16_t radius){
+    display_clear_circle(x,y,radius);
+}
+
+void screen_fill_circle(int16_t x, int16_t y, int16_t radius){
+    display_fill_circle(x,y,radius);
+}
+
+/******************************************************************************/
+/**                       Screen Text Display Functions                      **/
+/**                                                                          **/
+/**     These functions allow programmers to display text on the v5 screen   **/
+/******************************************************************************/
+
+void screen_print(text_format txt_fmt, const int16_t line, const char* text){
+    switch(txt_fmt){
+        case medium:
+        {
+            display_puts(line, text);
+            break;
+        }
+        case large:
+        {
+            display_big_puts(line, text);
+            break;
+        }
+        case medium_center:
+        {
+            display_center_puts(line, text);
+            break;
+        }
+        case large_center:
+        {
+            display_center_big_puts(line, text);
+            break;
+        }
+        default:
+        {
+            display_puts(line, text);
+            break;
+        }
+    }
+}
+
+void screen_print_at(text_format txt_fmt, int16_t x, int16_t y, const char* text){
+    switch(txt_fmt){
+        case small:
+        {
+            display_small_puts_at(x, y, text);
+            break;
+        }
+        case medium:
+        case medium_center:
+        {
+            display_puts_at(x, y, text);
+            break;
+        }
+        case large:
+        case large_center:
+        {
+            display_big_puts_at(x, y, text);
+            break;
+        }
+        default:
+        {
+            display_puts_at(x, y, text);
+            break;
+        }
+    }
+}
+
+void screen_printf(text_format txt_fmt, const int16_t line, const char* text, ...){
+    va_list args;
+	va_start(args, text);
+    switch(txt_fmt){
+        case medium:
+        {
+            display_vprintf(line, text, args);
+            break;
+        }
+        case large:
+        {
+            display_big_vprintf(line, text, args);
+            break;
+        }
+        case medium_center:
+        {
+            display_center_vprintf(line, text, args);
+            break;
+        }
+        case large_center:
+        {
+            display_center_big_vprintf(line, text, args);
+            break;
+        }
+        default:
+        {
+            display_vprintf(line, text, args);
+            break;
+        }
+    }
+}
+
+void screen_printf_at(text_format txt_fmt, int16_t x, int16_t y, const char* text, ...){
+    va_list args;
+	va_start(args, text);
+    switch(txt_fmt){
+        case small:
+        {
+            display_vprintf_at(x, y, text, args);
+            break;
+        }
+        case medium:
+        case medium_center:
+        {
+            display_vprintf_at(x, y, text, args);
+            break;
+        }
+        case large:
+        case large_center:
+        {
+            display_big_vprintf_at(x, y, text, args);
+            break;
+        }
+        default:
+        {
+            display_vprintf_at(x, y, text, args);
+            break;
+        }
+    }
+}
+
+void screen_vprintf(text_format txt_fmt, const int16_t line, const char* fmt, va_list args){
+    switch(txt_fmt){
+        case medium:
+        {
+            display_vprintf(line, fmt, args);
+            break;
+        }
+        case large:
+        {
+            display_big_vprintf(line, fmt, args);
+            break;
+        }
+        case medium_center:
+        {
+            display_center_vprintf(line, fmt, args);
+            break;
+        }
+        case large_center:
+        {
+            display_center_big_vprintf(line, fmt, args);
+            break;
+        }
+        default:
+        {
+            display_vprintf(line, fmt, args);
+            break;
+        }
+    }
+}
+
+void screen_vprintf_at(text_format txt_fmt, int16_t x, int16_t y, const char* fmt, va_list args){
+    switch(txt_fmt){
+        case small:
+        {
+            display_vprintf_at(x, y, fmt, args);
+            break;
+        }
+        case medium:
+        case medium_center:
+        {
+            display_vprintf_at(x, y, fmt, args);
+            break;
+        }
+        case large:
+        case large_center:
+        {
+            display_big_vprintf_at(x, y, fmt, args);
+            break;
+        }
+        default:
+        {
+            display_vprintf_at(x, y, fmt, args);
+            break;
+        }
+    }
+}
+
+/******************************************************************************/
+/**                         Screen Touch Functions                           **/
+/**                                                                          **/
+/**               These functions allow programmers to access                **/
+/**                    information about screen touches                      **/
+/******************************************************************************/
+
+int16_t screen_last_x(void){
+    return(touch_last_x());
+}
+
+int16_t screen_last_y(void){
+    return(touch_last_x());
+}
+
+touch_last_status screen_last_touch_status(void){
+    return(touch_last_event());
+}
