eb9490e9beabaa937f74483fbe73c29fcaab13bb
Will Xu <54247087+WillXuCodes@users.noreply.github.com>
2020-09-13 03:47:52 -0400

    🐛 Fix controller_clear_line (#226)
    * -Cleaned up repeating code using a macro in controller.c.
    -Added a test implementation for clearing controller text by passing a 0
    length/size string (as jpearman said on VEXForum is supposed to do:
    https://www.vexforum.com/t/pros-controller-screen-problem/83186/5?u=ranchsauce).
    
    * Fixed compiling errors.
    
    * Another few fixes relating to the one above (and bringing the number of
    chars back up to 19 max)
    
    * Version Reversion
    
    * Fixed formatting issues
    
    * Reverted version properly (forgot to save file last time)
    
    * Added vexSystemVersion check to controller_clear_line
    
    * Added old fix back in case user is on older version
    
    * Version reversion
    
    * Update version
    
    * Update src/devices/controller.c
    
    Co-authored-by: Alex Brooke <akb.sbc@gmail.com>

--- src/devices/controller.c
+++ src/devices/controller.c
@@ -38,21 +38,7 @@ void set_button_pressed(int port, int button, bool state) {
 
 int32_t controller_is_connected(controller_id_e_t id) {
 	uint8_t port;
-	switch (id) {
-		case E_CONTROLLER_MASTER:
-			port = V5_PORT_CONTROLLER_1;
-			break;
-		case E_CONTROLLER_PARTNER:
-			port = V5_PORT_CONTROLLER_2;
-			break;
-		default:
-			errno = EINVAL;
-			return PROS_ERR;
-	}
-	if (!internal_port_mutex_take(port)) {
-		errno = EACCES;
-		return PROS_ERR;
-	}
+	CONTROLLER_PORT_MUTEX_TAKE(id, port)
 	int32_t rtn = vexControllerConnectionStatusGet(id);
 	internal_port_mutex_give(port);
 	return rtn;
@@ -60,21 +46,7 @@ int32_t controller_is_connected(controller_id_e_t id) {
 
 int32_t controller_get_analog(controller_id_e_t id, controller_analog_e_t channel) {
 	uint8_t port;
-	switch (id) {
-		case E_CONTROLLER_MASTER:
-			port = V5_PORT_CONTROLLER_1;
-			break;
-		case E_CONTROLLER_PARTNER:
-			port = V5_PORT_CONTROLLER_2;
-			break;
-		default:
-			errno = EINVAL;
-			return PROS_ERR;
-	}
-	if (!internal_port_mutex_take(port)) {
-		errno = EACCES;
-		return PROS_ERR;
-	}
+	CONTROLLER_PORT_MUTEX_TAKE(id, port)
 	int32_t rtn = vexControllerGet(id, channel);
 	internal_port_mutex_give(port);
 	return rtn;
@@ -82,21 +54,7 @@ int32_t controller_get_analog(controller_id_e_t id, controller_analog_e_t channe
 
 int32_t controller_get_battery_capacity(controller_id_e_t id) {
 	uint8_t port;
-	switch (id) {
-		case E_CONTROLLER_MASTER:
-			port = V5_PORT_CONTROLLER_1;
-			break;
-		case E_CONTROLLER_PARTNER:
-			port = V5_PORT_CONTROLLER_2;
-			break;
-		default:
-			errno = EINVAL;
-			return PROS_ERR;
-	}
-	if (!internal_port_mutex_take(port)) {
-		errno = EACCES;
-		return PROS_ERR;
-	}
+	CONTROLLER_PORT_MUTEX_TAKE(id, port)
 	int32_t rtn = vexControllerGet(id, BatteryCapacity);
 	internal_port_mutex_give(port);
 	return rtn;
@@ -104,21 +62,7 @@ int32_t controller_get_battery_capacity(controller_id_e_t id) {
 
 int32_t controller_get_battery_level(controller_id_e_t id) {
 	uint8_t port;
-	switch (id) {
-		case E_CONTROLLER_MASTER:
-			port = V5_PORT_CONTROLLER_1;
-			break;
-		case E_CONTROLLER_PARTNER:
-			port = V5_PORT_CONTROLLER_2;
-			break;
-		default:
-			errno = EINVAL;
-			return PROS_ERR;
-	}
-	if (!internal_port_mutex_take(port)) {
-		errno = EACCES;
-		return PROS_ERR;
-	}
+	CONTROLLER_PORT_MUTEX_TAKE(id, port)
 	int32_t rtn = vexControllerGet(id, BatteryLevel);
 	internal_port_mutex_give(port);
 	return rtn;
@@ -126,21 +70,7 @@ int32_t controller_get_battery_level(controller_id_e_t id) {
 
 int32_t controller_get_digital(controller_id_e_t id, controller_digital_e_t button) {
 	uint8_t port;
-	switch (id) {
-		case E_CONTROLLER_MASTER:
-			port = V5_PORT_CONTROLLER_1;
-			break;
-		case E_CONTROLLER_PARTNER:
-			port = V5_PORT_CONTROLLER_2;
-			break;
-		default:
-			errno = EINVAL;
-			return PROS_ERR;
-	}
-	if (!internal_port_mutex_take(port)) {
-		errno = EACCES;
-		return PROS_ERR;
-	}
+	CONTROLLER_PORT_MUTEX_TAKE(id, port)
 	// the buttons enum starts at 4, the correct place for the libv5rts
 	int32_t rtn = vexControllerGet(id, button);
 	internal_port_mutex_give(port);
@@ -150,21 +80,7 @@ int32_t controller_get_digital(controller_id_e_t id, controller_digital_e_t butt
 int32_t controller_get_digital_new_press(controller_id_e_t id, controller_digital_e_t button) {
 	int32_t pressed = controller_get_digital(id, button);
 	uint8_t port;
-	switch (id) {
-		case E_CONTROLLER_MASTER:
-			port = V5_PORT_CONTROLLER_1;
-			break;
-		case E_CONTROLLER_PARTNER:
-			port = V5_PORT_CONTROLLER_2;
-			break;
-		default:
-			errno = EINVAL;
-			return PROS_ERR;
-	}
-	if (!internal_port_mutex_take(port)) {
-		errno = EACCES;
-		return PROS_ERR;
-	}
+	CONTROLLER_PORT_MUTEX_TAKE(id, port)
 	uint8_t button_num = button - E_CONTROLLER_DIGITAL_L1;
 
 	if (!pressed) {
@@ -184,21 +100,7 @@ int32_t controller_get_digital_new_press(controller_id_e_t id, controller_digita
 
 int32_t controller_set_text(controller_id_e_t id, uint8_t line, uint8_t col, const char* str) {
 	uint8_t port;
-	switch (id) {
-		case E_CONTROLLER_MASTER:
-			port = V5_PORT_CONTROLLER_1;
-			break;
-		case E_CONTROLLER_PARTNER:
-			port = V5_PORT_CONTROLLER_2;
-			break;
-		default:
-			errno = EINVAL;
-			return PROS_ERR;
-	}
-	if (!internal_port_mutex_take(port)) {
-		errno = EACCES;
-		return PROS_ERR;
-	}
+	CONTROLLER_PORT_MUTEX_TAKE(id, port)
 	line++;
 	if (col >= CONTROLLER_MAX_COLS)
 		col = CONTROLLER_MAX_COLS;
@@ -220,21 +122,7 @@ int32_t controller_set_text(controller_id_e_t id, uint8_t line, uint8_t col, con
 
 int32_t controller_print(controller_id_e_t id, uint8_t line, uint8_t col, const char* fmt, ...) {
 	uint8_t port;
-	switch (id) {
-		case E_CONTROLLER_MASTER:
-			port = V5_PORT_CONTROLLER_1;
-			break;
-		case E_CONTROLLER_PARTNER:
-			port = V5_PORT_CONTROLLER_2;
-			break;
-		default:
-			errno = EINVAL;
-			return PROS_ERR;
-	}
-	if (!internal_port_mutex_take(port)) {
-		errno = EACCES;
-		return PROS_ERR;
-	}
+	CONTROLLER_PORT_MUTEX_TAKE(id, port)
 	line++;
 	if (col >= CONTROLLER_MAX_COLS)
 		col = CONTROLLER_MAX_COLS;
@@ -260,9 +148,19 @@ int32_t controller_print(controller_id_e_t id, uint8_t line, uint8_t col, const 
 }
 
 int32_t controller_clear_line(controller_id_e_t id, uint8_t line) {
-	static const char* clear = "                   ";
-	kassert(strlen(clear) == CONTROLLER_MAX_COLS);
-	return controller_print(id, line, 0, clear);
+	uint8_t port;
+	CONTROLLER_PORT_MUTEX_TAKE(id, port);
+	int32_t rtn;
+	if (vexSystemVersion() >= 0x1000C38) {
+		static char* clear = "";
+		rtn = vexControllerTextSet(id, ++line, 1, clear);
+	} else {
+		static char* clear = "                   ";
+		kassert(strlen(clear) == CONTROLLER_MAX_COLS);
+		rtn = vexControllerTextSet(id, ++line, 1, clear);
+	}
+	internal_port_mutex_give(port);
+	return rtn;
 }
 
 int32_t controller_clear(controller_id_e_t id) {
