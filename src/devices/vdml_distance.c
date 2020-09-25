dda2bbfbd96384f95f0c7db7fa6ea02358d0bfd0
WillXuCodes <Will Xu>
2020-09-25 18:29:42 -0400

    Added bad port or device error handling (Needs review)

--- src/devices/vdml_distance.c
+++ src/devices/vdml_distance.c
@@ -16,36 +16,40 @@
 #include "vdml/registry.h"
 #include "vdml/vdml.h"
 
-#define ERROR_IMU_BAD_PORT(port, device, err_return)                  \
-	if (vexDeviceImuStatusGet(device->device_info) & E_IMU_STATUS_CALIBRATING) { \
+#define ERROR_DISTANCE_BAD_PORT(port, err_return)                  \
+	if (vexDistanceStatusGet(port) == &0x82 || vexDistanceStatusGet(port) == &0x82) { \
 		errno = EAGAIN;                                                            \
 		return_port(port - 1, err_return);                                         \
 	}
 
 int32_t distance_get(uint8_t port) {
 	claim_port_i(port - 1, E_DEVICE_DISTANCE);
+	ERROR_DISTANCE_BAD_PORT(port, PROS_ERR);
 	int32_t rtn = vexDistanceDistanceGet(port - 1);
 	return_port(port - 1, rtn);
 }
 
 int32_t distance_get_confidence(uint8_t port) {
 	claim_port_i(port - 1, E_DEVICE_DISTANCE);
+	ERROR_DISTANCE_BAD_PORT(port, PROS_ERR);
 	int32_t rtn = vexDistanceConfidenceGet(port - 1);
 	return_port(port - 1, rtn);
 }
 
 int32_t distance_get_object_size(uint8_t port){
 	claim_port_i(port - 1, E_DEVICE_DISTANCE);
+	ERROR_DISTANCE_BAD_PORT(port, PROS_ERR);
 	int32_t rtn = vexDistanceObjectSizeGet(port - 1);
 	return_port(port - 1, rtn);
 }
 
 double distance_get_object_velocity(uint8_t port){
 	claim_port_i(port - 1, E_DEVICE_DISTANCE);
+	ERROR_DISTANCE_BAD_PORT(port, PROS_ERR);
 	int32_t rtn = vexDistanceObjectVelocityGet(port - 1);
 	return_port(port - 1, rtn);
 }
 
-//TODO: Potentially add Error Handling (vexDistanceStatusGet, return error if bad)
+// TODO: Potentially add Error Handling to actual method calls
 // This may not be needed though because claim_port_i might already handle it... 
 // (Will look into this later)
