2ee04ea37b3b718cd0e049eb3f74d2e03371bacb
Kunwar Sahni <kunwar.sahni01@gmail.com>
2020-09-25 11:40:51 -0400

    Add get confidence

--- include/pros/distance.h
+++ include/pros/distance.h
@@ -45,6 +45,26 @@ namespace c {
 int32_t distance_get(uint8_t port);
 
 /**
+ * Get the confidence in the distance reading
+ * 
+ * This is a value that has a range of 0 to 63. 63 means high confidence, 
+ * lower values imply less confidence. Confidence is only available 
+ * when distance is > 200mm.
+ *
+ * This function uses the following values of errno when an error state is
+ * reached:
+ * ENXIO - The given value is not within the range of V5 ports (1-21).
+ * ENODEV - The port cannot be configured as an Distance Sensor
+ *
+ * \param  port
+ * 				 The V5 Distance Sensor port number from 1-21
+ * \return The confidence value or PROS_ERR_F if the operation failed, setting
+ * errno.
+ */
+
+int32_t distance_get_confidence(uint8_t port);
+
+/**
  * Get the current guess at relative object size
  *
  * This is a value that has a range of 0 to 400.
