95f5e81898d76f165587adebb9644cd67c9bba1e
Will Xu <54247087+WillXuCodes@users.noreply.github.com>
2020-10-27 20:16:14 -0400

    Update include/pros/optical.h
    Co-authored-by: Alex Brooke <akb.sbc@gmail.com>

--- include/pros/optical.h
+++ include/pros/optical.h
@@ -118,7 +118,7 @@ double optical_get_brightness(uint8_t port);
 /**
  * Get the detected proximity value
  *
- * This is not avaliable if gestures are being detected. proximity has
+ * This is not available if gestures are being detected. proximity has
  * a range of 0 to 255.
  *
  * This function uses the following values of errno when an error state is
