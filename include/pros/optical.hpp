55efaf4d10485bafdbcca00b465a3542ddf1b15a
Will Xu <54247087+WillXuCodes@users.noreply.github.com>
2020-10-27 20:16:43 -0400

    Update include/pros/optical.hpp
    Co-authored-by: Alex Brooke <akb.sbc@gmail.com>

--- include/pros/optical.hpp
+++ include/pros/optical.hpp
@@ -60,7 +60,7 @@ class Optical {
 	/**
 	 * Get the detected color saturation
 	 *
-	 * This is not avaliable if gestures are being detected. Saturation has a
+	 * This is not available if gestures are being detected. Saturation has a
 	 * range of 0 to 1.0
 	 *
 	 * This function uses the following values of errno when an error state is
