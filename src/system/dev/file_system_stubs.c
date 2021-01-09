9823196caa2f006f1be5473d10e0f48c6ee2553f
Kunwar Sahni <kunwar.sahni01@gmail.com>
2021-01-09 00:09:16 -0500

    Add missing file system stubs

--- src/system/dev/file_system_stubs.c
+++ src/system/dev/file_system_stubs.c
@@ -56,3 +56,28 @@ int _stat(const char* file, struct stat* st) {
 	errno = ENOSYS;
 	return -1;
 }
+
+int symlink(const char* file, const char* linkpath) {
+	errno = ENOSYS;
+	return -1;
+}
+
+ssize_t readlink(const char* pathname, char* buf, size_t bufsiz) {
+	errno = ENOSYS;
+	return NULL;
+}
+
+int truncate(const char* path, off_t length) {
+	errno = ENOSYS;
+	return -1;
+}
+
+int fchmod(int fd, mode_t mode) {
+	errno = ENOSYS;
+	return -1;
+}
+
+int fchmodat(int dirfd, const char* pathname, mode_t mode, int flags) {
+	errno = ENOSYS;
+	return -1;
+}
