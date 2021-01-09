1a2a40f4f60d3e6aedce36232b4753755d189ba0
Kunwar Sahni <kunwar.sahni01@gmail.com>
2021-01-09 07:41:13 -0500

    Put fchmod and fchmodat by chmod

--- src/system/dev/file_system_stubs.c
+++ src/system/dev/file_system_stubs.c
@@ -32,6 +32,16 @@ int chmod(const char* pathname, mode_t mode) {
 	return -1;
 }
 
+int fchmod(int fd, mode_t mode) {
+	errno = ENOSYS;
+	return -1;
+}
+
+int fchmodat(int dirfd, const char* pathname, mode_t mode, int flags) {
+	errno = ENOSYS;
+	return -1;
+}
+
 long pathconf(const char* path, int name) {
 	errno = ENOSYS;
 	return -1;
@@ -65,20 +75,9 @@ int symlink(const char* file, const char* linkpath) {
 ssize_t readlink(const char* pathname, char* buf, size_t bufsiz) {
 	errno = ENOSYS;
 	return -1;
-
 }
 
 int truncate(const char* path, off_t length) {
-	errno = ENOSYS;
-	return -1;
-}
-
-int fchmod(int fd, mode_t mode) {
-	errno = ENOSYS;
-	return -1;
-}
-
-int fchmodat(int dirfd, const char* pathname, mode_t mode, int flags) {
 	errno = ENOSYS;
 	return -1;
 }
