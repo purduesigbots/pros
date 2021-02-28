1a424b9b6f55dcbc2c8bea1b7efaa8844b40bfe8
Andrew Strauss <astrauss11@gmail.com>
2021-02-28 15:34:00 -0500

    Add necessary include for VEXos function

--- src/rtos/tasks.c
+++ src/rtos/tasks.c
@@ -29,6 +29,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "v5_api.h"
+
 /* Defining MPU_WRAPPERS_INCLUDED_FROM_API_FILE prevents task.h from redefining
 all the API functions to use the MPU wrappers.  That should only be done when
 task.h is included from an application file. */
