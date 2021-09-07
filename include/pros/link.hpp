30063e7588e3e515f82e1f0771d52a3b001e845c
WillXuCodes <xu.z.william@gmail.com>
2021-09-07 14:44:58 -0400

    Initial Header File Creation

---
+++ include/pros/link.hpp
@@ -0,0 +1,17 @@
+/**
+ * \file pros/link.hpp
+ *
+ * Contains prototypes for functions related to robot to robot communications.
+ *
+ * Visit https://pros.cs.purdue.edu/v5/tutorials/topical/link.html to learn
+ * more.
+ *
+ * This file should not be modified by users, since it gets replaced whenever
+ * a kernel upgrade occurs.
+ *
+ * Copyright (c) 2017-2021, Purdue University ACM SIGBots.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
