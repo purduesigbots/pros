4fe8b0e74ed62419f544f27ab81108551ec08fcd
Will Xu <54247087+WillXuCodes@users.noreply.github.com>
2021-10-22 13:33:32 -0400

    🚸 Linked List TODO Comment Updates/Warnings (#235)
    * Added function vs data flags for linked list nodes
    
    * Put version back to old release version
    
    * Removed some changes
    
    * Added warning comments
    
    * Reverted More Changes
    
    * Removed random whitespace

--- src/common/linkedlist.c
+++ src/common/linkedlist.c
@@ -18,6 +18,9 @@
 #include "common/linkedlist.h"
 #include "kapi.h"
 
+// NOTE: Do not intermix data and function payloads. This may cause data to be 
+// re-evaluated as a pointer to an area in memory and a false free or add.
+
 ll_node_s_t* linked_list_init_func_node(generic_fn_t func) {
 	ll_node_s_t* node = (ll_node_s_t*)kmalloc(sizeof *node);
 	node->payload.func = func;
@@ -75,8 +78,6 @@ void linked_list_append_func(linked_list_s_t* list, generic_fn_t func) {
 	it->next = n;
 }
 
-// NOTE: this will likely break if the list has intermixed data and function ptrs
-// TODO: add tag field to ll_node_s_t to easily check what is present
 void linked_list_remove_func(linked_list_s_t* list, generic_fn_t func) {
 	if (list == NULL || list->head == NULL) return;
 
@@ -113,8 +114,6 @@ void linked_list_append_data(linked_list_s_t* list, void* data) {
 	it->next = n;
 }
 
-// NOTE: this will likely break if the list has intermixed data and function ptrs
-// TODO: add tag field to ll_node_s_t to easily check what is present
 void linked_list_remove_data(linked_list_s_t* list, void* data) {
 	if (list == NULL || list->head == NULL) return;
 
