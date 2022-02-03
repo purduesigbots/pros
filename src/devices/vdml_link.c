3ad298240fe2a98ee67abdd889d830acb9711551
AyushShukla3 <ayushshukla30339@gmail.com>
2022-02-03 15:41:29 -0500

    transmist protocol

--- src/devices/vdml_link.c
+++ src/devices/vdml_link.c
@@ -24,6 +24,7 @@
 
 #define COBS_HEADER_SIZE    0   // Placeholder, Fill in later once protocol implemented
 #define FIFO_SIZE           512 // VEX Defined TX/RX Queue Size
+#define HEADER_SIZE         3 // Header size for transmission
 
 // other info about vexlink discovered:
 // E_DEVICE_GENERIC is the "Generic Serial device, but also a vexlink device
@@ -87,9 +88,9 @@ uint32_t link_readable_size(uint8_t port) {
 
 uint32_t link_transmit_raw(uint8_t port, void* data, uint32_t data_size) {
     claim_port_i(port - 1, E_DEVICE_SERIAL);
-    if(!vexDeviceGenericRadioLinkStatus(device->device_info)) {
         errno = ENXIO;
         return_port(port - 1, PROS_ERR);
+    if(!vexDeviceGenericRadioLinkStatus(device->device_info)) {
     }
     if(data_size > vexDeviceGenericRadioWriteFree(device->device_info)) {
         errno = EBUSY;
@@ -99,7 +100,28 @@ uint32_t link_transmit_raw(uint8_t port, void* data, uint32_t data_size) {
         errno = EINVAL;
         return_port(port - 1, PROS_ERR);
     }
-    vexDeviceGenericRadioTransmit(device->device_info, (uint8_t*)data, (uint8_t)data_size);
+
+    uint8_t* msg;
+    uint8_t msg_size = HEADER_SIZE;
+
+    // data start indicator
+    msg[0] = 0x33;
+    // data size
+    msg[1] = (uint16_t)data_size;
+
+    // message
+    for (int i = 0; i < data_size; i++) {
+        msg[msg_size] = ((char*)data)[i];
+        msg_size++;
+    }
+
+    // checksum
+    for (int j = 0; j < data_size; j++) {
+        msg[msg_size] = (uint8_t)(((char*)data)[j] ^ ((char*)data)[j]);
+        msg_size++;
+    }
+
+    vexDeviceGenericRadioTransmit(device->device_info, (uint8_t*)msg, (uint8_t)msg_size);
     return_port(port - 1, 1);
 }
 
@@ -117,7 +139,34 @@ uint32_t link_read_raw(uint8_t port, void* dest, uint32_t size) {
         errno = EINVAL;
         return_port(port - 1, PROS_ERR);
     }
+
     uint32_t rtv = vexDeviceGenericRadioReceive(device->device_info, dest, size);
+
+    uint8_t* data;
+    uint16_t data_size = (uint16_t)((uint8_t*)dest)[1];
+
+    if (((uint8_t*)dest)[0] == 0x33) {
+        dest = -1;
+        return_port(port - 1, -1);
+    }
+
+    // store data
+    for (int i = HEADER_SIZE; i < data_size; i++)
+        data[i - HEADER_SIZE] = ((uint8_t*)dest)[i];
+    
+    // checksum
+    for (int i = 0; i < data_size; i++) {
+        if (((uint8_t*)dest)[data_size + HEADER_SIZE + i] != (uint8_t)((char*)data)[data_size + HEADER_SIZE] ^ ((char*)data)[data_size + HEADER_SIZE]) {
+            dest = -1;
+            return_port(port - 1, -1);
+        }
+    }
+
+    // put data into result
+    for (int i = HEADER_SIZE; i < data_size; i++) {
+        ((uint8_t*)dest)[i] = data[i - HEADER_SIZE];
+    }
+
     return_port(port - 1, rtv);
 }
 
@@ -126,4 +175,3 @@ uint32_t link_tx_size_raw(uint8_t port) {
     uint32_t rtv = vexDeviceGenericRadioWriteFree(device->device_info);
     return_port(port - 1, rtv);
 }
-
