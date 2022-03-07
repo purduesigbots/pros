286f14c2a7981319d39e8e9be9335698b909d5b0
omegaStag <kathyniu2003@gmail.com>
2022-03-07 18:47:30 -0500

    Update tasks.c

--- src/rtos/tasks.c
+++ src/rtos/tasks.c
@@ -4751,7 +4751,7 @@ uint32_t uxReturn;
 
 	void task_join(task_t task) {
     		task_notify_when_deleting(task, NULL, 1, E_NOTIFY_ACTION_INCR);
-    		while (!pros::Task::notify_take(true, 20));
+    		while (!task_notify_take(true, 20));
 	}
 
 #endif /* configUSE_TASK_NOTIFICATIONS */
