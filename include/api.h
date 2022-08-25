1df8094251b27731f05097b552ca90eaea487c9e
Will Xu <54247087+WillXuCodes@users.noreply.github.com>
2022-08-24 21:39:23 -0400

    Revert API and version

--- include/api.h
+++ include/api.h
@@ -41,8 +41,8 @@
 
 #define PROS_VERSION_MAJOR 3
 #define PROS_VERSION_MINOR 6
-#define PROS_VERSION_PATCH 3
-#define PROS_VERSION_STRING "3.6.3-dirty.50.f9c809e"
+#define PROS_VERSION_PATCH 2
+#define PROS_VERSION_STRING "3.6.2"
 
 #include "pros/adi.h"
 #include "pros/colors.h"
