f618d3ea1f93cf87d2a2dde63ad74c8106c23834
Cole <cole@colex.dev>
2022-10-20 18:51:39 -0400

    Object Oriented Rotation

--- include/pros/rotation.hpp
+++ include/pros/rotation.hpp
@@ -24,6 +24,7 @@
 #include <cstdint>
 
 #include "pros/rotation.h"
+#include "rtos.hpp"
 
 namespace pros {
 inline namespace v5 {
@@ -56,7 +57,7 @@ class Rotation {
 	 * \return 1 if the operation was successful or PROS_ERR if the operation
 	 * failed, setting errno.
 	 */
-	virtual std::int32_t reset();
+	virtual std::int32_t reset() const;
 
 	/**
 	 * Set the Rotation Sensor's refresh interval in milliseconds.
@@ -94,7 +95,7 @@ class Rotation {
 	 * \return 1 if the operation was successful or PROS_ERR if the operation
 	 * failed, setting errno.
 	 */
-	virtual std::int32_t set_position(std::uint32_t position);
+	virtual std::int32_t set_position(std::uint32_t position) const;
 
 	/**
 	 * Reset the Rotation Sensor to a desired rotation value
@@ -109,7 +110,7 @@ class Rotation {
 	 * \return 1 if the operation was successful or PROS_ERR if the operation
 	 * failed, setting errno.
 	 */
-	virtual std::int32_t reset_position(void);
+	virtual std::int32_t reset_position(void) const;
 
 	/**
 	 * Get the Rotation Sensor's current position in centidegrees
@@ -122,7 +123,7 @@ class Rotation {
 	 * \return The position value or PROS_ERR if the operation failed, setting
 	 * errno.
 	 */
-	virtual std::int32_t get_position();
+	virtual std::int32_t get_position() const;
 
 	/**
 	 * Get the Rotation Sensor's current velocity in centidegrees per second
@@ -138,7 +139,7 @@ class Rotation {
 	 value or PROS_ERR_F if the operation failed, setting
 	 * errno.
 	 */
-	virtual std::int32_t get_velocity();
+	virtual std::int32_t get_velocity() const;
 
 	/**
 	 * Get the Rotation Sensor's current position in centidegrees
@@ -151,7 +152,7 @@ class Rotation {
 	 * \return The angle value or PROS_ERR if the operation failed, setting
 	 * errno.
 	 */
-	virtual std::int32_t get_angle();
+	virtual std::int32_t get_angle() const;
 
 	/**
 	 * Set the Rotation Sensor's direction reversed flag
@@ -168,7 +169,7 @@ class Rotation {
 	 * \return 1 if the operation was successful or PROS_ERR if the operation
 	 * failed, setting errno.
 	 */
-	virtual std::int32_t set_reversed(bool value);
+	virtual std::int32_t set_reversed(bool value) const;
 
 	/**
 	 * Reverse the Rotation Sensor's direction.
@@ -181,7 +182,7 @@ class Rotation {
 	 * \return 1 if the operation was successful or PROS_ERR if the operation
 	 * failed, setting errno.
 	 */
-	virtual std::int32_t reverse();
+	virtual std::int32_t reverse() const;
 
 	/**
 	 * Get the Rotation Sensor's reversed flag
@@ -194,8 +195,13 @@ class Rotation {
 	 * \return Reversed value or PROS_ERR if the operation failed, setting
 	 * errno.
 	 */
-	virtual std::int32_t get_reversed();
+	virtual std::int32_t get_reversed() const ;
 	///@}
+    
+    private:
+    mutable bool _reverse_flag;
+	mutable pros::Mutex _rotation_mutex;
+    virtual void push_rotation_configuration(void) const;
 };
 }
 }  // namespace pros
--- src/devices/vdml_rotation.cpp
+++ src/devices/vdml_rotation.cpp
@@ -14,50 +14,65 @@
 
 namespace pros {
 inline namespace v5 {
-    
+
 Rotation::Rotation(const std::uint8_t port, const bool reverse_flag) : _port(port) {
 	pros::c::rotation_init_reverse(port, reverse_flag);
 }
 
-std::int32_t Rotation::reset() {
+std::int32_t Rotation::reset() const {
+	push_rotation_configuration();
 	return pros::c::rotation_reset(_port);
 }
 
 std::int32_t Rotation::set_data_rate(std::uint32_t rate) const {
+	push_rotation_configuration();
 	return pros::c::rotation_set_data_rate(_port, rate);
 }
 
-std::int32_t Rotation::set_position(std::uint32_t position) {
+std::int32_t Rotation::set_position(std::uint32_t position) const {
+	push_rotation_configuration();
 	return pros::c::rotation_set_position(_port, position);
 }
 
-std::int32_t Rotation::reset_position(void) {
+std::int32_t Rotation::reset_position(void) const {
+	push_rotation_configuration();
 	return pros::c::rotation_reset_position(_port);
 }
 
-std::int32_t Rotation::get_position(void) {
-    return pros::c::rotation_get_position(_port);
+std::int32_t Rotation::get_position(void) const {
+	push_rotation_configuration();
+	return pros::c::rotation_get_position(_port);
 }
 
-std::int32_t Rotation::get_velocity(void) {
-    return pros::c::rotation_get_velocity(_port);
+std::int32_t Rotation::get_velocity(void) const {
+	push_rotation_configuration();
+	return pros::c::rotation_get_velocity(_port);
 }
 
-std::int32_t Rotation::get_angle(void) {
-    return pros::c::rotation_get_angle(_port);
+std::int32_t Rotation::get_angle(void) const {
+	push_rotation_configuration();
+	return pros::c::rotation_get_angle(_port);
 }
 
-std::int32_t Rotation::set_reversed(bool value) {
-    return pros::c::rotation_set_reversed(_port, value);
+std::int32_t Rotation::set_reversed(bool value) const {
+	_reverse_flag = value;
+	return pros::c::rotation_set_reversed(_port, value);
 }
 
-std::int32_t Rotation::reverse(void) {
-    return pros::c::rotation_reverse(_port);
+std::int32_t Rotation::reverse(void) const {
+	push_rotation_configuration();
+	return pros::c::rotation_reverse(_port);
 }
 
-std::int32_t Rotation::get_reversed(void) {
-    return pros::c::rotation_get_reversed(_port);
+std::int32_t Rotation::get_reversed(void) const {
+	return _reverse_flag;
 }
 
+void Rotation::push_rotation_configuration() const {
+	_rotation_mutex.take();
+	set_reversed(_reverse_flag);
+	_rotation_mutex.give();
 }
+
+}  // namespace v5
 }  // namespace pros
