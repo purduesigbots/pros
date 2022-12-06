41e5f639c6b50fb48d8ebd450bc0b94a28d13858
Grace Lu <lu986@purdue.edu>
2022-12-06 00:32:29 -0500

    Edit methods of pneumatics class

--- src/devices/vdml_adi.cpp
+++ src/devices/vdml_adi.cpp
@@ -217,12 +217,12 @@ Pneumatics::Pneumatics(std::uint8_t adi_port, bool initial_state) : DigitalOut(a
 Pneumatics::Pneumatics(ext_adi_port_pair_t port_pair, bool initial_state) : DigitalOut(std::get<1>(port_pair)), state(initial_state) {
 }
 
-std::int32_t Pneumatics::extend() {
+std::int32_t Pneumatics::extend() const {
 	state = true;
 	return set_value(state);
 }
 
-std::int32_t Pneumatics::retract() {
+std::int32_t Pneumatics::retract() const {
 	state = false;
 	return set_value(state);
 }
