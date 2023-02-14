242e813ca304bb46ac2621f8b3bcdf7f0c1f4213
ryan-doan <doanngocdung01@gmail.com>
2023-02-14 18:30:02 -0500

    Revert changes to api.h and version due to pros make

--- include/api.h
+++ include/api.h
@@ -39,10 +39,10 @@
 #include <unistd.h>
 #endif /* __cplusplus */
 
-#define PROS_VERSION_MAJOR 3
-#define PROS_VERSION_MINOR 7
-#define PROS_VERSION_PATCH 2
-#define PROS_VERSION_STRING "3.7.2-dirty.104.b1d0433"
+#define PROS_VERSION_MAJOR 4
+#define PROS_VERSION_MINOR 0
+#define PROS_VERSION_PATCH 0
+#define PROS_VERSION_STRING "4.0.0"
 
 #include "pros/adi.h"
 #include "pros/colors.h"
