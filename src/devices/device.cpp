4df7f6b0b94f0fb60a64d3c377541b8aa5773188
aberiggs <sprocket710@gmail.com>
2023-02-21 22:20:32 -0500

    Fix errors in device.cpp

--- src/devices/device.cpp
+++ src/devices/device.cpp
@@ -5,37 +5,18 @@
 
 namespace pros {
 inline namespace v5 {
-class Device {
-	public:
-	/**
-	 * Gets the port number of the Smart Device.
-	 *
-	 * \return The smart device's port number.
-	 */
-	std::uint8_t get_port(void) {
+
+	std::uint8_t Device::get_port(void) {
 		return _port;
 	}
 
-	/**
-	 * Gets the type of device.
-	 *
-	 * \return The device type as an enum.
-	 */
-	virtual pros::DeviceType get_type() {
+	
+	pros::DeviceType Device::get_type() {
         return DeviceType::motor;
     }
 
-	protected:
-	/**
-	 * Creates a Device object.
-	 *
-	 * \param port The V5 port number from 1-21
-	 *
-	 */
-	explicit Device(const std::uint8_t port) : _port(port) {}
+	Device::Device(const std::uint8_t port) : _port(port) {}
 
-	protected:
-	const std::uint8_t _port;
-};
+
 }  // namespace v5
 }  // namespace p
