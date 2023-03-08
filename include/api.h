7c893ffe8c3e56bf053981a1566c92b6f1f1c3bd
Richard Stump <rnstump@purdue.edu>
2023-03-07 19:48:41 -0500

    Update version

--- include/api.h
+++ include/api.h
@@ -40,9 +40,9 @@
 #endif /* __cplusplus */
 
 #define PROS_VERSION_MAJOR 3
-#define PROS_VERSION_MINOR 7
-#define PROS_VERSION_PATCH 3
-#define PROS_VERSION_STRING "3.7.3"
+#define PROS_VERSION_MINOR 8
+#define PROS_VERSION_PATCH 0
+#define PROS_VERSION_STRING "3.8.0"
 
 #include "pros/adi.h"
 #include "pros/colors.h"
