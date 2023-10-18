02cf79bf37eb71346ec7deaed742011f76f86748
noam987 <noam@arie11.com>
2023-10-18 17:39:55 -0400

    Update version numbers

--- include/api.h
+++ include/api.h
@@ -41,8 +41,8 @@
 
 #define PROS_VERSION_MAJOR 4
 #define PROS_VERSION_MINOR 0
-#define PROS_VERSION_PATCH 2
-#define PROS_VERSION_STRING "4.0.2"
+#define PROS_VERSION_PATCH 6
+#define PROS_VERSION_STRING "4.0.6"
 
 #include "pros/adi.h"
 #include "pros/colors.h"
