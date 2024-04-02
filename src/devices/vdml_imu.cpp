f7e1bb3b5030718d4295761b6b55a912859a2925
Your Name <you@example.com>
2024-04-01 20:19:07 -0700

    Fix IMU is_calibrating and get_orientation when unplugged

--- src/devices/vdml_imu.cpp
+++ src/devices/vdml_imu.cpp
@@ -63,7 +63,11 @@ pros::c::imu_status_e_t Imu::get_status() const {
 }
 
 bool Imu::is_calibrating() const {
-	return get_status() & pros::c::E_IMU_STATUS_CALIBRATING;
+	pros::c::imu_status_e_t status = get_status();
+	if (status == pros::c::E_IMU_STATUS_ERROR) {
+		return false;
+	}
+	return status & pros::c::E_IMU_STATUS_CALIBRATING;
 }
 
 std::int32_t Imu::tare_heading() const {
