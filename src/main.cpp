2fea1aeca58d3c85464e9944cb5b644a99bad5d0
noam987 <noam@arie11.com>
2024-04-04 16:01:15 -0400

    revert main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -75,15 +75,20 @@ void autonomous() {}
  */
 void opcontrol() {
 	pros::Controller master(pros::E_CONTROLLER_MASTER);
-	pros::Imu imu = pros::Imu::getImu();
+	pros::MotorGroup left_mg({1, -2, 3});    // Creates a motor group with forwards ports 1 & 3 and reversed port 2
+	pros::MotorGroup right_mg({-4, 5, -6});  // Creates a motor group with forwards port 4 and reversed ports 4 & 6
 
-	pros::Imu im2 = pros::Imu::getImu();
-
-	m2.move(127);
 	while (true) {
-		printf("IMU port %d, heading: %f\n", imu.get_port(), imu.get_heading());
-		// Arcade control scheme
+		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
+		                 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
 
-		pros::delay(20);  // Run for 20 ms then update
+		                 (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);  // Prints status of the emulated screen LCDs
+
+		// Arcade control scheme
+		int dir = master.get_analog(ANALOG_LEFT_Y);    // Gets amount forward/backward from left joystick
+		int turn = master.get_analog(ANALOG_RIGHT_X);  // Gets the turn left/right from right joystick
+		left_mg = dir - turn;                          // Sets left motor voltage
+		right_mg = dir + turn;                         // Sets right motor voltage
+		pros::delay(20);                               // Run for 20 ms then update
 	}
 }
