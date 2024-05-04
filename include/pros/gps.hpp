0636b48f55d7cdebe2dca2c473d15d1f8b520f27
noam987 <50681033+noam987@users.noreply.github.com>
2024-05-04 18:20:19 -0400

    ✨Adds static getters for some devices (#653)
    * imu_v1
    
    * revert main.cpp
    
    * Add injector for gps
    
    * rename to _ casing and add vision sensor

--- include/pros/gps.hpp
+++ include/pros/gps.hpp
@@ -12,7 +12,7 @@
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
- * 
+ *
  * \defgroup cpp-gps VEX GPS Sensor C API
  * \note For a pros-specific usage guide on the GPS, please check out our article [here.](@ref gps)
  */
@@ -25,8 +25,8 @@
 #include <cstdint>
 #include <iostream>
 
-#include "pros/gps.h"
 #include "pros/device.hpp"
+#include "pros/gps.h"
 
 namespace pros {
 inline namespace v5 {
@@ -41,7 +41,6 @@ class Gps : public Device {
 	 */
 
 	public:
-
 	/**
 	 * Creates a GPS object for the given port.
 	 *
@@ -61,8 +60,7 @@ class Gps : public Device {
 	 */
 	Gps(const std::uint8_t port) : Device(port, DeviceType::gps){};
 
-	Gps(const Device& device)
-		: Gps(device.get_port()) {};
+	Gps(const Device& device) : Gps(device.get_port()){};
 
 	/**
 	 * Creates a GPS object for the given port.
@@ -88,7 +86,8 @@ class Gps : public Device {
 	 * \endcode
 	 *
 	 */
-	explicit Gps(const std::uint8_t port, double xInitial, double yInitial, double headingInitial) : Device(port, DeviceType::gps){
+	explicit Gps(const std::uint8_t port, double xInitial, double yInitial, double headingInitial)
+	    : Device(port, DeviceType::gps) {
 		pros::c::gps_set_position(port, xInitial, yInitial, headingInitial);
 	};
 
@@ -108,19 +107,19 @@ class Gps : public Device {
 	 * \param yOffset
 	 * 				 Cartesian 4-Quadrant Y offset from center of turning (meters)
 	 *
-	 * \b Example: 
+	 * \b Example:
 	 * \code
 	 * pros::Gps gps(1, 1.30, 1.20);
 	 * \endcode
 	 *
 	 */
-	explicit Gps(const std::uint8_t port, double xOffset, double yOffset) : Device(port, DeviceType::gps){
+	explicit Gps(const std::uint8_t port, double xOffset, double yOffset) : Device(port, DeviceType::gps) {
 		pros::c::gps_set_offset(port, xOffset, yOffset);
 	};
 
 	/**
 	 * Creates a GPS object for the given port.
-	 * 
+	 *
 	 * This function uses the following values of errno when an error state is
 	 * reached:
 	 * ENXIO - The given value is not within the range of V5 ports (1-21).
@@ -146,8 +145,9 @@ class Gps : public Device {
 	 * \endcode
 	 *
 	 */
-	explicit Gps(const std::uint8_t port, double xInitial, double yInitial, double headingInitial, double xOffset, double yOffset)
-	     : Device(port, DeviceType::gps){
+	explicit Gps(const std::uint8_t port, double xInitial, double yInitial, double headingInitial, double xOffset,
+	             double yOffset)
+	    : Device(port, DeviceType::gps) {
 		pros::c::gps_initialize_full(port, xInitial, yInitial, headingInitial, xOffset, yOffset);
 	};
 
@@ -226,34 +226,34 @@ class Gps : public Device {
 	static std::vector<Gps> get_all_devices();
 
 	/**
-	* Get the GPS's cartesian location relative to the center of turning/origin in meters.
-	*
-	* This function uses the following values of errno when an error state is
-	* reached:
-	* ENXIO - The given value is not within the range of V5 ports (1-21).
-	* ENODEV - The port cannot be configured as a GPS
-	* EAGAIN - The sensor is still calibrating
-	*
-	* \param  port
-	* 				 The V5 GPS port number from 1-21
-	* \return A struct (gps_position_s_t) containing the X and Y values if the operation
-	* failed, setting errno.
-	* 
-	* \b Example
-	* \code
-	* #define GPS_PORT 1
-	* 
-	* void opcontrol() {
-	*   gps_position_s_t pos;
-	* 	Gps gps(GPS_PORT);
-	*   while (true) {
-	*     pos = gps.get_offset();
-	*     screen_print(TEXT_MEDIUM, 1, "X Offset: %4d, Y Offset: %4d", pos.x, pos.y);
-	*     delay(20);
-	*   }
-	* }
-	* \endcode
-	*/
+	 * Get the GPS's cartesian location relative to the center of turning/origin in meters.
+	 *
+	 * This function uses the following values of errno when an error state is
+	 * reached:
+	 * ENXIO - The given value is not within the range of V5 ports (1-21).
+	 * ENODEV - The port cannot be configured as a GPS
+	 * EAGAIN - The sensor is still calibrating
+	 *
+	 * \param  port
+	 * 				 The V5 GPS port number from 1-21
+	 * \return A struct (gps_position_s_t) containing the X and Y values if the operation
+	 * failed, setting errno.
+	 *
+	 * \b Example
+	 * \code
+	 * #define GPS_PORT 1
+	 *
+	 * void opcontrol() {
+	 *   gps_position_s_t pos;
+	 * 	Gps gps(GPS_PORT);
+	 *   while (true) {
+	 *     pos = gps.get_offset();
+	 *     screen_print(TEXT_MEDIUM, 1, "X Offset: %4d, Y Offset: %4d", pos.x, pos.y);
+	 *     delay(20);
+	 *   }
+	 * }
+	 * \endcode
+	 */
 	virtual pros::gps_position_s_t get_offset() const;
 
 	/**
@@ -273,7 +273,7 @@ class Gps : public Device {
 	 *  			 Heading with 0 being north on the field, in degrees [0,360) going clockwise
 	 * \return 1 if the operation was successful or PROS_ERR if the operation
 	 * failed, setting errno.
-	 * 
+	 *
 	 * \b Example
 	 * \code
 	 * #define GPS_PORT 1
@@ -282,7 +282,7 @@ class Gps : public Device {
 	 * 	Gps gps(GPS_PORT);
 	 *  gps.set_position(1.3, 1.4, 180);
 	 *   while (true) {
-	 *     printf("X: %f, Y: %f, Heading: %f\n", gps.get_position().x, 
+	 *     printf("X: %f, Y: %f, Heading: %f\n", gps.get_position().x,
 	 * 	   gps.get_position().y, gps.get_position().heading);
 	 *     delay(20);
 	 *   }
@@ -361,7 +361,7 @@ class Gps : public Device {
 	 * \return A struct (gps_status_s_t) containing values mentioned above.
 	 * If the operation failed, all the structure's members are filled with
 	 * PROS_ERR_F and errno is set.
-	 * 
+	 *
 	 * \b Example
 	 * \code
 	 * #define GPS_PORT 1
@@ -410,7 +410,7 @@ class Gps : public Device {
 	 */
 	virtual pros::gps_position_s_t get_position() const;
 
-    /**
+	/**
 	 * Gets the X position in meters of the robot relative to the starting position.
 	 *
 	 * This function uses the following values of errno when an error state is
@@ -421,7 +421,7 @@ class Gps : public Device {
 	 *
 	 * \return The X position in meters. If the operation failed,
 	 * returns PROS_ERR_F and errno is set.
-	 * 
+	 *
 	 * \b Example
 	 * \code
 	 * #define GPS_PORT 1
@@ -436,11 +436,11 @@ class Gps : public Device {
 	 * }
 	 * \endcode
 	 */
-    virtual double get_position_x() const;
+	virtual double get_position_x() const;
 
-    /**
+	/**
 	 * Gets the Y position in meters of the robot relative to the starting position.
-	 * 
+	 *
 	 * This function uses the following values of errno when an error state is
 	 * reached:
 	 * ENXIO - The given value is not within the range of V5 ports (1-21).
@@ -449,7 +449,7 @@ class Gps : public Device {
 	 *
 	 * \return The Y position in meters. If the operation failed,
 	 * returns PROS_ERR_F and errno is set.
-	 * 
+	 *
 	 * \b Example
 	 * \code
 	 * #define GPS_PORT 1
@@ -466,9 +466,9 @@ class Gps : public Device {
 	 */
 	virtual double get_position_y() const;
 
-    /**
+	/**
 	 * Gets the pitch, roll, and yaw of the GPS relative to the starting orientation.
-     * 
+	 *
 	 * This function uses the following values of errno when an error state is
 	 * reached:
 	 * ENXIO - The given value is not within the range of V5 ports (1-21).
@@ -488,16 +488,16 @@ class Gps : public Device {
 	 *  gps_orientation_s_t orientation;
 	 *   while (true) {
 	 *	 orientation = gps.get_orientation();
-	 *     printf("pitch: %f, roll: %f, yaw: %f\n", orientation.pitch, 
+	 *     printf("pitch: %f, roll: %f, yaw: %f\n", orientation.pitch,
 	 *         orientation.roll, orientation.yaw);
 	 *     delay(20);
 	 *   }
 	 * }
 	 * \endcode
-	*/
+	 */
 	virtual pros::gps_orientation_s_t get_orientation() const;
 
-    /**
+	/**
 	 * Gets the pitch of the robot in degrees relative to the starting oreintation.
 	 *
 	 * This function uses the following values of errno when an error state is
@@ -508,7 +508,7 @@ class Gps : public Device {
 	 *
 	 * \return The pitch in [0,360) degree values. If the operation failed,
 	 * returns PROS_ERR_F and errno is set.
-	 * 
+	 *
 	 * \b Example
 	 * \code
 	 * #define GPS_PORT 1
@@ -525,7 +525,7 @@ class Gps : public Device {
 	 */
 	virtual double get_pitch() const;
 
-    /**
+	/**
 	 * Gets the roll of the robot in degrees relative to the starting oreintation.
 	 *
 	 * This function uses the following values of errno when an error state is
@@ -536,7 +536,7 @@ class Gps : public Device {
 	 *
 	 * \return The roll in [0,360) degree values. If the operation failed,
 	 * returns PROS_ERR_F and errno is set.
-	 * 
+	 *
 	 * \b Example
 	 * \code
 	 * #define GPS_PORT 1
@@ -564,7 +564,7 @@ class Gps : public Device {
 	 *
 	 * \return The yaw in [0,360) degree values. If the operation failed,
 	 * returns PROS_ERR_F and errno is set.
-	 * 
+	 *
 	 * \b Example
 	 * \code
 	 * #define GPS_PORT 1
@@ -639,7 +639,7 @@ class Gps : public Device {
 	virtual double get_heading_raw() const;
 
 	/**
-	 * Get the GPS's raw gyroscope values
+	 * Get the GPS's raw gyroscope value in z-axis
 	 *
 	 * This function uses the following values of errno when an error state is
 	 * reached:
@@ -647,18 +647,18 @@ class Gps : public Device {
 	 * ENODEV - The port cannot be configured as a GPS
 	 * EAGAIN - The sensor is still calibrating
 	 *
-	 * \return The raw gyroscope values. If the operation failed, all the
-	 * structure's members are filled with PROS_ERR_F and errno is set.
+	 * \return The raw gyroscope value in z-axis. If the operation fails, returns
+	 * PROS_ERR_F and errno is set.
 	 *
-	 * \b Example
+	 \b Example
 	 * \code
 	 * #define GPS_PORT 1
 	 *
 	 * void opcontrol() {
 	 * 	Gps gps(GPS_PORT);
 	 *  while(true) {
-	 *		pros::gps_gyro_s_t gyro = gps.get_gyro_rate();
-	 *		printf("Gyro: %f, %f, %f\n", gyro.x, gyro.y, gyro.z);
+	 *		double gyro_z = gps.get_gyro_z();
+	 *		printf("gyro_z: %f\n", gyro_z);
 	 *  	pros::delay(20);
 	 *  }
 	 * }
@@ -668,16 +668,16 @@ class Gps : public Device {
 
 	/**
 	 * Get the GPS's raw gyroscope value in x-axis
-	 * 
+	 *
 	 * This function uses the following values of errno when an error state is
 	 * reached:
 	 * ENXIO - The given value is not within the range of V5 ports (1-21).
 	 * ENODEV - The port cannot be configured as a GPS
 	 * EAGAIN - The sensor is still calibrating
-	 * 
+	 *
 	 * \return The raw gyroscope value in x-axis. If the operation fails, returns
 	 * PROS_ERR_F and errno is set.
-	 * 
+	 *
 	 * \b Example
 	 * \code
 	 * #define GPS_PORT 1
@@ -691,21 +691,21 @@ class Gps : public Device {
 	 *  }
 	 * }
 	 * \endcode
-	*/
+	 */
 	virtual double get_gyro_rate_x() const;
 
-    /**
+	/**
 	 * Get the GPS's raw gyroscope value in y-axis
-	 * 
+	 *
 	 * This function uses the following values of errno when an error state is
 	 * reached:
 	 * ENXIO - The given value is not within the range of V5 ports (1-21).
 	 * ENODEV - The port cannot be configured as a GPS
 	 * EAGAIN - The sensor is still calibrating
-	 * 
+	 *
 	 * \return The raw gyroscope value in y-axis. If the operation fails, returns
 	 * PROS_ERR_F and errno is set.
-	 * 
+	 *
 	 * \b Example
 	 * \code
 	 * #define GPS_PORT 1
@@ -719,21 +719,21 @@ class Gps : public Device {
 	 *  }
 	 * }
 	 * \endcode
-	*/
+	 */
 	virtual double get_gyro_rate_y() const;
 
 	/**
 	 * Get the GPS's raw gyroscope value in z-axis
-	 * 
+	 *
 	 * This function uses the following values of errno when an error state is
 	 * reached:
 	 * ENXIO - The given value is not within the range of V5 ports (1-21).
 	 * ENODEV - The port cannot be configured as a GPS
 	 * EAGAIN - The sensor is still calibrating
-	 * 
+	 *
 	 * \return The raw gyroscope value in z-axis. If the operation fails, returns
 	 * PROS_ERR_F and errno is set.
-	 * 
+	 *
 	 \b Example
 	 * \code
 	 * #define GPS_PORT 1
@@ -766,16 +766,16 @@ class Gps : public Device {
 
 	/**
 	 * Get the GPS's raw accelerometer value in x-axis
-	 * 
+	 *
 	 * This function uses the following values of errno when an error state is
 	 * reached:
 	 * ENXIO - The given value is not within the range of V5 ports (1-21).
 	 * ENODEV - The port cannot be configured as an GPS
 	 * EAGAIN - The sensor is still calibrating
-	 * 
+	 *
 	 * \return The raw accelerometer value in x-axis. If the operation fails, returns
 	 * PROS_ERR_F and errno is set.
-	 * 
+	 *
 	 * \b Example
 	 * \code
 	 * #define GPS_PORT 1
@@ -789,21 +789,21 @@ class Gps : public Device {
 	 *  }
 	 * }
 	 * \endcode
-	*/
-    virtual double get_accel_x() const;
+	 */
+	virtual double get_accel_x() const;
 
 	/**
 	 * Get the GPS's raw accelerometer value in y-axis
-	 * 
+	 *
 	 * This function uses the following values of errno when an error state is
 	 * reached:
 	 * ENXIO - The given value is not within the range of V5 ports (1-21).
 	 * ENODEV - The port cannot be configured as an GPS
 	 * EAGAIN - The sensor is still calibrating
-	 * 
+	 *
 	 * \return The raw accelerometer value in y-axis. If the operation fails, returns
 	 * PROS_ERR_F and errno is set.
-	 * 
+	 *
 	 * \b Example
 	 * \code
 	 * #define GPS_PORT 1
@@ -817,21 +817,21 @@ class Gps : public Device {
 	 *  }
 	 * }
 	 * \endcode
-	*/
+	 */
 	virtual double get_accel_y() const;
 
-    /**
+	/**
 	 * Get the GPS's raw accelerometer value in z-axis
-	 * 
+	 *
 	 * This function uses the following values of errno when an error state is
 	 * reached:
 	 * ENXIO - The given value is not within the range of V5 ports (1-21).
 	 * ENODEV - The port cannot be configured as an GPS
 	 * EAGAIN - The sensor is still calibrating
-	 * 
+	 *
 	 * \return The raw accelerometer value in z-axis. If the operation fails, returns
 	 * PROS_ERR_F and errno is set.
-	 * 
+	 *
 	 * \b Example
 	 * \code
 	 * #define GPS_PORT 1
@@ -845,12 +845,12 @@ class Gps : public Device {
 	 *  }
 	 * }
 	 * \endcode
-	*/
+	 */
 	virtual double get_accel_z() const;
 
 	/**
 	 * This is the overload for the << operator for printing to streams
-	 * 
+	 *
 	 * Prints in format:
 	 * Gps [port: gps._port, x: (x position), y: (y position), heading: (gps heading), rotation: (gps rotation)]
 	 *
@@ -869,35 +869,56 @@ class Gps : public Device {
 	 */
 	friend std::ostream& operator<<(std::ostream& os, const pros::Gps& gps);
 
-///@}
+	/**
+	 * Gets a gps sensor that is plugged in to the brain
+	 *
+	 * \note The first time this function is called it returns the gps sensor at the lowest port
+	 * If this function is called multiple times, it will cycle through all the ports.
+	 * For example, if you have 1 gps sensor on the robot
+	 * this function will always return a gps sensor object for that port.
+	 * If you have 2 gps sensors, all the odd numered calls to this function will return objects
+	 * for the lower port number,
+	 * all the even number calls will return gps objects for the higher port number
+	 *
+	 *
+	 * This functions uses the following values of errno when an error state is
+	 * reached:
+	 * ENODEV - No gps sensor is plugged into the brain
+	 *
+	 * \return A gps object corresponding to a port that a gps sensor is connected to the brain
+	 * If no gps sensor is plugged in, it returns a gps sensor on port PROS_ERR_BYTE
+	 *
+	 */
+	static Gps get_gps();
+	///@}
 };  // Gps Class
 
 namespace literals {
-	/**
-	 * Constructs a Gps object with the given port number
-	 * 
-	 * \b Example
-	 * \code
-	 * using namespace literals;
-	 * 
-	 * void opcontrol() {
-	 * 	 pros::Gps gps = 1_gps;
-	 *   while (true) {
-	 *     pos = gps.get_position();
-	 *     screen_print(TEXT_MEDIUM, 1, "X Position: %4d, Y Position: %4d", pos.x, pos.y);
-	 *     delay(20);
-	 *   }
-	 * }
-	 * \endcode
-	 */
-	const pros::Gps operator""_gps(const unsigned long long int g);
+/**
+ * Constructs a Gps object with the given port number
+ *
+ * \b Example
+ * \code
+ * using namespace literals;
+ *
+ * void opcontrol() {
+ * 	 pros::Gps gps = 1_gps;
+ *   while (true) {
+ *     pos = gps.get_position();
+ *     screen_print(TEXT_MEDIUM, 1, "X Position: %4d, Y Position: %4d", pos.x, pos.y);
+ *     delay(20);
+ *   }
+ * }
+ * \endcode
+ */
+const pros::Gps operator""_gps(const unsigned long long int g);
 }  // namespace literals
 
 /// @brief
 /// Alias for Gps is GPS for user convenience.
 using GPS = Gps;
 
-} // namespace v5
-} // namespace pros
+}  // namespace v5
+}  // namespace pros
 
 #endif
