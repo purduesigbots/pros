9bc0e95059597e0ee45c8823511fa979b81be61c
Rocky14683 <rocky10208@gmail.com>
2024-05-15 15:09:33 +0800

    Fix a typo in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -76,7 +76,7 @@ void autonomous() {}
 void opcontrol() {
 	pros::Controller master(pros::E_CONTROLLER_MASTER);
 	pros::MotorGroup left_mg({1, -2, 3});    // Creates a motor group with forwards ports 1 & 3 and reversed port 2
-	pros::MotorGroup right_mg({-4, 5, -6});  // Creates a motor group with forwards port 4 and reversed ports 4 & 6
+	pros::MotorGroup right_mg({-4, 5, -6});  // Creates a motor group with forwards port 5 and reversed ports 4 & 6
 
 	while (true) {
 		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
