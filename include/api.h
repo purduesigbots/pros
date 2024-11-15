dd00a673552a1e26e1f9c6420f6e51cda75c87c0
Andrew Lu <lu987@purdue.edu>
2024-11-15 12:15:17 -0500

    bump version number

--- include/api.h
+++ include/api.h
@@ -42,8 +42,8 @@
 #define PROS_VERSION_MAJOR 4
 #define PROS_VERSION_MINOR 1
 
-#define PROS_VERSION_PATCH 0
-#define PROS_VERSION_STRING "4.1.0"
+#define PROS_VERSION_PATCH 1
+#define PROS_VERSION_STRING "4.1.1"
 
 
 #include "pros/adi.h"
