bea9c443195fbecb738a4154f2048603fa4dd178
ion098 <146852218+ion098@users.noreply.github.com>
2024-11-19 10:54:48 -0800

    refactor: :recycle: remove unnecessary base detection code, add header guard, add tests

--- include/pros/detail.hpp
+++ include/pros/detail.hpp
@@ -13,6 +13,8 @@
  * file You can obtain one at http://mozilla.org/MPL/2.0/.
  *
  */
+#ifndef _PROS_DETAIL_HPP_
+#define _PROS_DETAIL_HPP_
 
 #include <array>
 #include <charconv>
@@ -22,30 +24,9 @@ namespace pros::detail {
 template <char ...Chars>
 consteval uint8_t parse_port() {
     const std::array chars{Chars...};
-    const char* str = chars.begin();
-    int base = 10;
     uint8_t port_num{0};
-    if (chars.size() > 2 && chars[0] == '0') {
-        str += 2;
-        // from_chars doesn't seem to handle hex/binary/octal prefixes,
-        // so we handle them ourselves
-        switch (chars[1]) {
-            case 'X': case 'x':
-                // hex literal
-                base = 16;
-                break;
-            case 'B': case 'b':
-                // binary literal
-                base = 2;
-                break;
-            default:
-                // octal literal
-                str -= 1;
-                base = 8;
-        }
-    }
-    auto result = std::from_chars(str, chars.end(), port_num, base);
-    if (result.ptr != chars.end()) {
+    auto result = std::from_chars(chars.begin(), chars.end(), port_num, 0);
+    if (result.ptr != chars.end() || result.ec != std::errc{}) {
         return 0;
     }
     return port_num;
@@ -56,4 +37,23 @@ consteval bool is_valid_port() {
     auto port_num = parse_port<Chars...>();
     return port_num >= 1 && port_num <= 21;
 }
+
+// Tests:
+static_assert(is_valid_port<'1'>() == true, "Kernel test failed");
+static_assert(is_valid_port<'0'>() == false, "Kernel test failed");
+// 0x16 = 22
+static_assert(is_valid_port<'0','x','1','6'>() == false, "Kernel test failed");
+// 0x15 = 21
+static_assert(is_valid_port<'0','x','1','5'>() == true, "Kernel test failed");
+// 025 = 21
+static_assert(is_valid_port<'0','2','5'>() == true, "Kernel test failed");
+// 0b1 = 1
+static_assert(is_valid_port<'0','b','1'>() == true, "Kernel test failed");
+// 0x1 = 1
+static_assert(is_valid_port<'0','x','1'>() == true, "Kernel test failed");
+static_assert(is_valid_port<'-','1'>() == false, "Kernel test failed");
+static_assert(is_valid_port<'2','2'>() == false, "Kernel test failed");
+
 }
+
+#endif // _PROS_DETAIL_HPP_
