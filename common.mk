b8d2af7dc5c58cefa868e79eb9a1bf7b04f5152b
ion098 <146852218+ion098@users.noreply.github.com>
2025-03-26 17:51:52 -0700

    refactor: :zap: Reduce code binary size (#708)
    * refactor: :alembic: attempt to remove unneeded sections in linkerscript
    
    * feat: :alembic: switch to thumb mode
    
    * bug: :alembic: use arm mode in unwind.c
    
    * refactor: :alembic: fix thumb-arm interworking
    
    * fix: :pencil2: fix misspelled attribute
    
    * docs: :bulb: Explain the removal of stack in linkerscript
    
    * build: :bug: Remove unsupported compiler flag
    
    * docs: :bulb: Explain why target("arm") attribute is needed
    
    * feat: :alembic: Try reducing code size by removing -mthumb-interwork
    
    * feat: :zap: More linker script optimizations
    
    Remove uneeded sections, and sort sections by alignment to reduce padding
    
    * fix: :bug: Move flags to correct makefile variable
    
    ---------
    
    Co-authored-by: Andrew Lu <lu987@purdue.edu>

--- common.mk
+++ common.mk
@@ -1,7 +1,7 @@
 ARCHTUPLE=arm-none-eabi-
 DEVICE=VEX EDR V5
 
-MFLAGS=-mcpu=cortex-a9 -mfpu=neon-fp16 -mfloat-abi=hard -Os -g
+MFLAGS=-mcpu=cortex-a9 -mfpu=neon-fp16 -mfloat-abi=hard -Os -g -mthumb
 CPPFLAGS=-D_POSIX_THREADS -D_UNIX98_THREAD_MUTEX_ATTRIBUTES -D_POSIX_TIMERS -D_POSIX_MONOTONIC_CLOCK
 GCCFLAGS=-ffunction-sections -fdata-sections -fdiagnostics-color -funwind-tables
 
@@ -34,7 +34,7 @@ LIBRARIES+=$(wildcard $(FWDIR)/*.a)
 EXCLUDE_COLD_LIBRARIES+=$(FWDIR)/libc.a $(FWDIR)/libm.a
 COLD_LIBRARIES=$(filter-out $(EXCLUDE_COLD_LIBRARIES), $(LIBRARIES))
 wlprefix=-Wl,$(subst $(SPACE),$(COMMA),$1)
-LNK_FLAGS=--gc-sections --start-group $(strip $(LIBRARIES)) -lgcc -lstdc++ --end-group -T$(FWDIR)/v5-common.ld --no-warn-rwx-segments
+LNK_FLAGS=--gc-sections --start-group $(strip $(LIBRARIES)) -lgcc -lstdc++ --end-group -T$(FWDIR)/v5-common.ld --no-warn-rwx-segments --sort-section=alignment --sort-common
 
 ASMFLAGS=$(MFLAGS) $(WARNFLAGS)
 CFLAGS=$(MFLAGS) $(CPPFLAGS) $(WARNFLAGS) $(GCCFLAGS) --std=$(C_STANDARD)
