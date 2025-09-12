4f234b1d695cedc87a096664a662117b63414052
Ayaan B <ayaanbarbhuiya098@gmail.com>
2025-09-12 20:36:47 +0000

    ci: :zap: compile example code snippets in parallel

--- check_example_code.py
+++ check_example_code.py
@@ -2,6 +2,8 @@ import os
 import re
 import subprocess
 import sys
+import time
+import traceback
 
 include_directory = "include/pros"
 is_error = False
@@ -10,38 +12,96 @@ def print_and_exit(message):
     print(f"Failed to check example code in pros headers:\n{message}")
     sys.exit(1)
 
-def compile_code(code_text, is_cpp):
+max_subprocesses = os.cpu_count()
+current_subprocesses = 0
+subprocess_list = []
+
+def compile_code(code_text, filename, is_cpp):
     """
     Compiles the given code, discarding the output file.
     
     Args:
         code_text (str): The C/C++ source code to compile.
+        filename (str): The name of the file.
         is_cpp (bool): Whether the code is C++ or not.
-        
-    Returns:
-        (success: bool, stdout: str, stderr: str)
     """
+    global max_subprocesses, current_subprocesses, subprocess_list
+    while current_subprocesses == max_subprocesses:
+        for i in range(len(subprocess_list)):
+            proc, (code_snippet, filename) = subprocess_list[i]
+            if isinstance(proc, subprocess.Popen):
+                retcode = proc.poll()
+                if retcode is not None:
+                    current_subprocesses -= 1
+                    success = (retcode == 0)
+                    if not success:
+                        global is_error
+                        is_error = True
+                    stdout, stderr = proc.stdout.read(), proc.stderr.read()
+                    subprocess_list[i] = (success, stdout, stderr), (code_snippet, filename)
+        time.sleep(1)
+    while len(subprocess_list) > 0 and not isinstance(subprocess_list[0][0], subprocess.Popen):
+        (success, stdout, stderr), (code_snippet, filename) = subprocess_list.pop(0)
+        if not success:
+            print(f"=== example code from {filename} failed to compile: ===")
+            print(code_snippet)
+            print("=== compiler output below: ===")
+            print(stderr)
     flags = [
         "arm-none-eabi-gcc",
         "-x", "c++" if is_cpp else "c",
         "-std=c++23" if is_cpp else "-std=c2x",
         "-I", "include",
         "-D", "_PROS_KERNEL_SUPPRESS_LLEMU_WARNING",
+        "-Wfatal-errors",
         "-",                                         # Read input from stdin
         "-S",                                        # Generate assembly (avoids linker errors)
         "-o", os.devnull                             # Discard the output file
     ]
-
     process = subprocess.Popen(
         flags,
         stdin=subprocess.PIPE,
         stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE
+        stderr=subprocess.PIPE,
+        text=True
     )
-    stdout, stderr = process.communicate(input=code_text.encode("utf-8"))
-    success = (process.returncode == 0)
+    process.stdin.write(code_text)
+    process.stdin.close()
+    subprocess_list.append((process, (code_text, filename)))
+    current_subprocesses += 1
 
-    return success, stdout.decode(), stderr.decode()
+#def compile_code__(code_text, is_cpp):
+#    """
+#    Compiles the given code, discarding the output file.
+#    
+#    Args:
+#        code_text (str): The C/C++ source code to compile.
+#        is_cpp (bool): Whether the code is C++ or not.
+#        
+#    Returns:
+#        (success: bool, stdout: str, stderr: str)
+#    """
+#    flags = [
+#        "arm-none-eabi-gcc",
+#        "-x", "c++" if is_cpp else "c",
+#        "-std=c++23" if is_cpp else "-std=c2x",
+#        "-I", "include",
+#        "-D", "_PROS_KERNEL_SUPPRESS_LLEMU_WARNING",
+#        "-",                                         # Read input from stdin
+#        "-S",                                        # Generate assembly (avoids linker errors)
+#        "-o", os.devnull                             # Discard the output file
+#    ]
+#
+#    process = subprocess.Popen(
+#        flags,
+#        stdin=subprocess.PIPE,
+#        stdout=subprocess.PIPE,
+#        stderr=subprocess.PIPE
+#    )
+#    stdout, stderr = process.communicate(input=code_text.encode("utf-8"))
+#    success = (process.returncode == 0)
+#
+#    return success, stdout.decode(), stderr.decode()
 
 def check_example_code(filename, header_text, is_cpp):
     """
@@ -52,7 +112,7 @@ def check_example_code(filename, header_text, is_cpp):
         header_text (str): The content of the file.
         is_cpp (bool): Whether the file is C++ or not.
     """
-    print(f"checking example in {filename} ...")
+    print(f"checking all examples in {filename} ...", file=sys.stderr)
     # This pattern matches all lines between the \code and \endcode markers
     pattern = r"(^.+\\code\n)((.*\n)+?)(^.+\\endcode)"
     for match in re.finditer(pattern, header_text, re.MULTILINE):
@@ -61,14 +121,7 @@ def check_example_code(filename, header_text, is_cpp):
         # Remove the leading * from each line
         cleaned_lines = [re.sub(r"^\s*\*", "", line) for line in lines]
         code_snippet = "#include \"main.h\"\n"+"\n".join(cleaned_lines)
-        is_success, compiler_stdout, compiler_stderr = compile_code(code_snippet, is_cpp)
-        if not is_success:
-            print(f"=== example code from {filename} failed to compile: ===")
-            print(code_snippet)
-            print("=== compiler output below: ===")
-            print(compiler_stderr)
-            global is_error
-            is_error = True
+        compile_code(code_snippet, filename, is_cpp)
 
 try:
     for filename in os.listdir(include_directory):
