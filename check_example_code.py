2d9a547423024ed154159fdff2c8af448a240921
Ayaan B <ayaanbarbhuiya098@gmail.com>
2025-09-15 18:35:35 +0000

    ci: :zap: use asyncio, precompile main.h for c++

--- check_example_code.py
+++ check_example_code.py
@@ -1,139 +1,119 @@
+import asyncio
 import os
 import re
 import subprocess
 import sys
-import time
-import traceback
+import tempfile
 
 include_directory = "include/pros"
-is_error = False
+precompiled_include_directory = ""
+error_count = 0
+gcc_semaphore = asyncio.Semaphore(value=os.cpu_count())
 
 def print_and_exit(message):
-    print(f"Failed to check example code in pros headers:\n{message}")
+    print(f"Failed to check example code in pros headers:\n{message}", file=sys.stderr)
     sys.exit(1)
 
-max_subprocesses = os.cpu_count()
-current_subprocesses = 0
-subprocess_list = []
+def precompile_header():
+    flags = [
+        "-x", "c++-header",
+        "include/main.h",
+        "-std=c++23",
+        "-I", "include",
+        "-D", "_PROS_KERNEL_SUPPRESS_LLEMU_WARNING",
+        "-Wfatal-errors",
+        "-o", os.path.join(precompiled_include_directory, "main.h.gch")
+    ]
+    subprocess.run(["arm-none-eabi-gcc", *flags], check=True)
 
-def compile_code(code_text, filename, is_cpp):
+async def compile_code(code_text, filename, is_cpp):
     """
     Compiles the given code, discarding the output file.
     
     Args:
         code_text (str): The C/C++ source code to compile.
-        filename (str): The name of the file.
         is_cpp (bool): Whether the code is C++ or not.
+        
+    Returns:
+        success (bool): Whether the compilation succeeded or not.
     """
-    global max_subprocesses, current_subprocesses, subprocess_list
-    while current_subprocesses == max_subprocesses:
-        for i in range(len(subprocess_list)):
-            proc, (code_snippet, filename) = subprocess_list[i]
-            if isinstance(proc, subprocess.Popen):
-                retcode = proc.poll()
-                if retcode is not None:
-                    current_subprocesses -= 1
-                    success = (retcode == 0)
-                    if not success:
-                        global is_error
-                        is_error = True
-                    stdout, stderr = proc.stdout.read(), proc.stderr.read()
-                    subprocess_list[i] = (success, stdout, stderr), (code_snippet, filename)
-        time.sleep(1)
-    while len(subprocess_list) > 0 and not isinstance(subprocess_list[0][0], subprocess.Popen):
-        (success, stdout, stderr), (code_snippet, filename) = subprocess_list.pop(0)
-        if not success:
-            print(f"=== example code from {filename} failed to compile: ===")
-            print(code_snippet)
-            print("=== compiler output below: ===")
-            print(stderr)
     flags = [
-        "arm-none-eabi-gcc",
         "-x", "c++" if is_cpp else "c",
         "-std=c++23" if is_cpp else "-std=c2x",
+        # Only need precompiled headers for C++
+        *(("-I", precompiled_include_directory) if is_cpp else ()),
         "-I", "include",
+        # Prevent spurious warnings
         "-D", "_PROS_KERNEL_SUPPRESS_LLEMU_WARNING",
+        # Stop compilation on first error
         "-Wfatal-errors",
-        "-",                                         # Read input from stdin
-        "-S",                                        # Generate assembly (avoids linker errors)
-        "-o", os.devnull                             # Discard the output file
+        # Read input from stdin
+        "-",
+        # Generate assembly (avoids linker errors)
+        "-S",
+        # Discard the output file
+        "-o", os.devnull
     ]
-    process = subprocess.Popen(
-        flags,
-        stdin=subprocess.PIPE,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        text=True
-    )
-    process.stdin.write(code_text)
-    process.stdin.close()
-    subprocess_list.append((process, (code_text, filename)))
-    current_subprocesses += 1
+    async with gcc_semaphore:
+        process = await asyncio.create_subprocess_exec(
+            "arm-none-eabi-gcc",
+            *flags,
+            stdin=asyncio.subprocess.PIPE,
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.PIPE
+        )
+        stdout, stderr = await process.communicate(input=code_text.encode("utf-8"))
+        success = (process.returncode == 0)
 
-#def compile_code__(code_text, is_cpp):
-#    """
-#    Compiles the given code, discarding the output file.
-#    
-#    Args:
-#        code_text (str): The C/C++ source code to compile.
-#        is_cpp (bool): Whether the code is C++ or not.
-#        
-#    Returns:
-#        (success: bool, stdout: str, stderr: str)
-#    """
-#    flags = [
-#        "arm-none-eabi-gcc",
-#        "-x", "c++" if is_cpp else "c",
-#        "-std=c++23" if is_cpp else "-std=c2x",
-#        "-I", "include",
-#        "-D", "_PROS_KERNEL_SUPPRESS_LLEMU_WARNING",
-#        "-",                                         # Read input from stdin
-#        "-S",                                        # Generate assembly (avoids linker errors)
-#        "-o", os.devnull                             # Discard the output file
-#    ]
-#
-#    process = subprocess.Popen(
-#        flags,
-#        stdin=subprocess.PIPE,
-#        stdout=subprocess.PIPE,
-#        stderr=subprocess.PIPE
-#    )
-#    stdout, stderr = process.communicate(input=code_text.encode("utf-8"))
-#    success = (process.returncode == 0)
-#
-#    return success, stdout.decode(), stderr.decode()
+        if not success:
+            global error_count
+            error_count += 1
+            print(f"=== example code from {filename} failed to compile ===")
+            print(code_text)
+            print("=== compiler output below: ===")
+            print(stderr.decode())
+            print("========")
+        
+        return success
 
-def check_example_code(filename, header_text, is_cpp):
-    """
-    Checks all the example code snippets in the header file and ensures they compile.
+def example_code_generator():
+    try:
+        files = os.listdir(include_directory)
+    except:
+        print_and_exit(f"Error accessing directory '{include_directory}': {e}")
+    try:
+        for filename in files:
+            file_path = os.path.join(include_directory, filename)
+            if os.path.isfile(file_path):
+                try:
+                    with open(file_path, "r") as f:
+                        header_text = f.read()
+                        is_cpp = filename.endswith(".hpp")
+                        # This pattern matches all lines between the \code and \endcode markers
+                        pattern = r"(^.+\\code\n)((.*\n)+?)(^.+\\endcode)"
+                        for match in re.finditer(pattern, header_text, re.MULTILINE):
+                            code_block = match.group(2)
+                            lines = code_block.splitlines()
+                            # Remove the leading * from each line
+                            cleaned_lines = [re.sub(r"^\s*\* ?", "", line) for line in lines]
+                            code_snippet = "#include \"main.h\"\n"+"\n".join(cleaned_lines)
+                            yield code_snippet, filename, is_cpp
+                except Exception as e:
+                    print_and_exit(f"Error reading file '{filename}': {e}")
+    except Exception as e:
+        print_and_exit(f"Error accessing directory '{include_directory}': {e}")
 
-    Args:
-        filename (str): The name of the header file.
-        header_text (str): The content of the file.
-        is_cpp (bool): Whether the file is C++ or not.
-    """
-    print(f"checking all examples in {filename} ...", file=sys.stderr)
-    # This pattern matches all lines between the \code and \endcode markers
-    pattern = r"(^.+\\code\n)((.*\n)+?)(^.+\\endcode)"
-    for match in re.finditer(pattern, header_text, re.MULTILINE):
-        code_block = match.group(2)
-        lines = code_block.splitlines()
-        # Remove the leading * from each line
-        cleaned_lines = [re.sub(r"^\s*\*", "", line) for line in lines]
-        code_snippet = "#include \"main.h\"\n"+"\n".join(cleaned_lines)
-        compile_code(code_snippet, filename, is_cpp)
+async def main():
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        global precompiled_include_directory
+        precompiled_include_directory = tmpdirname
+        precompile_header()
+        async with asyncio.TaskGroup() as tg:
+            tasks = [tg.create_task(compile_code(*item)) for item in example_code_generator()]
+    results = [task.result() for task in tasks]
+    global error_count
+    if error_count > 0:
+        print_and_exit(f"{error_count} compile errors encountered")
 
-try:
-    for filename in os.listdir(include_directory):
-        file_path = os.path.join(include_directory, filename)
-        if os.path.isfile(file_path):
-            try:
-                with open(file_path, "r") as f:
-                    check_example_code(filename, f.read(), filename.endswith(".hpp"))
-            except Exception as e:
-                print_and_exit(f"Error reading file '{filename}': {e}")
-except Exception as e:
-    print_and_exit(f"Error accessing directory '{include_directory}': {e}")
-
-if is_error:
-    sys.exit(1)
+if __name__ == "__main__":
+    asyncio.run(main())
