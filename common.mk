90000090aefb8dc2238fc3a2a89111dfa2d849dd
Mayank Patibandla <34776435+mayankpatibandla@users.noreply.github.com>
2026-01-13 23:46:54 -0500

    update cxx standard

--- common.mk
+++ common.mk
@@ -20,8 +20,8 @@ WARNFLAGS+=-Wno-psabi
 SPACE := $() $()
 COMMA := ,
 
-C_STANDARD?=gnu2x
-CXX_STANDARD?=gnu++23
+C_STANDARD?=gnu23
+CXX_STANDARD?=gnu++26
 
 DEPDIR := .d
 $(shell mkdir -p $(DEPDIR))
