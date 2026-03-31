c0897ad1f70950ea7590424d4527ea7af7d32e51
Will Xu <54247087+WillXuCodes@users.noreply.github.com>
2026-03-31 11:03:09 -0700

    chore: 📝 Update Copyright Year (#788)
    * chore: 📝 Update Copyright Year
    
    * Forgot one

--- src/system/cpp_support.cpp
+++ src/system/cpp_support.cpp
@@ -3,7 +3,7 @@
  *
  * C++ support hooks
  *
- * \copyright (c) 2017-2024, Purdue University ACM SIGBots.
+ * \Copyright (c) 2017-2026, Purdue University ACM SIGBots.
  * All rights reserved.
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
